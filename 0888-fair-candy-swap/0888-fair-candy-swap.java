81d821bf1a7af50b8322c5965b8ec0c4a51ef11c
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-01-19 19:07:15 +0530

    Time: 8 ms (99.09%), Space: 43.9 MB (85.84%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.java
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -1,18 +1,28 @@
 class Solution {
     public int[] fairCandySwap(int[] A, int[] B) {
-        int sa = 0, sb = 0;  
-        for (int x: A) sa += x;
-        for (int x: B) sb += x;
-        int delta = (sb - sa) / 2;
-       
-
-        Set<Integer> setB = new HashSet();
-        for (int x: B) setB.add(x);
-
-        for (int x: A)
-            if (setB.contains(x + delta))
-                return new int[]{x, x + delta};
+        
+        int sa=0,sb=0;
+        for(int x : A) sa+=x;
+        for(int x : B) sb+=x;
+        
+        int d = sb-sa;
+        d=d/2;
+        
+        Set<Integer> S = new HashSet<>();
+        
+        for(int x: B)
+            S.add(x);
+        
+        for(int x : A)
+        {
+            if(S.contains(x+d))
+            {
+                return new int[]{x,x+d};
+            }
+        }
+        
         return null;
-
+        
+        
     }
 }
