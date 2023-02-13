a416087c485d4227394384e71bb8aebaca9072af
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-02-13 19:32:44 +0530

    Time: 9 ms (31.88%), Space: 38.9 MB (72.69%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        
+        return f(n);
+    }
+    static int f(int n)
+    {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        
+        return f(n-1)+f(n-2);
+    }
+}
