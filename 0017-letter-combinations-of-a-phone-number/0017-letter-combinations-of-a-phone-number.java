e8c4f3926f65d3a3929277fa8c7e7c3ef49c0ddd
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-02-16 20:07:11 +0530

    Time: 6 ms (31.35%), Space: 42.1 MB (33.52%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,62 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        
+        return pad("",digits);
+    }
+    List<String> pad(String p , String up)
+    {
+        if(up.isEmpty())
+        {
+            List<String> str = new ArrayList<String>();
+     if(p.isEmpty())
+     {
+         return str;
+     }
+            str.add(p);
+            return str;
+        }
+        
+        List<String> list = new ArrayList<>();
+        
+        
+        int digit = up.charAt(0) -'0';
+    
+        if(digit==7)
+        { 
+            
+            
+            for(int i=(digit-3)*3 + 3;i<(digit-2)*3+4;i++)
+            {
+                char ch = (char)('a'+i);
+                list.addAll(pad(p+ch,up.substring(1)));
+            }
+        }
+        else if(digit==8)
+        {
+           for(int i=(digit-3)*3 +4;i<(digit-3)*3+7;i++)
+            {
+                char ch = (char)('a'+i);
+                list.addAll(pad(p+ch,up.substring(1)));
+            } 
+        }
+         else  if(digit==9)
+        { 
+            
+            
+            for(int i=22;i<26;i++)
+            {
+                char ch = (char)('a'+i);
+                list.addAll(pad(p+ch,up.substring(1)));
+            }
+        }
+        else
+        {
+            for(int i=(digit-2)*3;i<(digit-1)*3;i++)
+            {
+                char ch = (char)('a'+i);
+                list.addAll(pad(p+ch,up.substring(1)));
+            }
+        }
+        return list;
+    }
+}
