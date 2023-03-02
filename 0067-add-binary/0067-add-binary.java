3497709dfecab79e5658030f1f62889b14c59342
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-03-02 20:35:49 +0530

    Time: 1 ms (100.00%), Space: 41.4 MB (47.17%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,126 +1,15 @@
-class Solution {
-    public String addBinary(String as, String bs) {
-        
- int i = as.length()-1 , j=bs.length()-1;
-        char a[] = as.toCharArray();
-        char b[] = bs.toCharArray();
-       StringBuffer res = new StringBuffer();
-           int carry=0; 
-        while(i>=0 && j>=0)
-        {
-        
-            if(a[i]=='0'&& b[j]=='0')
-            {
-                if(carry ==1)
-                {
-                    res.append('1');
-                    carry=0;
-                }
-                else
-                res.append('0');
-            }
-            
-            if(a[i]=='0'&&b[j]=='1')
-            {
-                if(carry ==1)
-                {
-                    res.append('0');
-                    carry=1;
-                }
-                else {res.append('1'); carry=0;} 
-            }
-            
-            if(a[i]== '1' && b[j]=='0')
-            {
-                if(carry ==1)
-                {
-                    res.append('0');
-                    carry=1;
-                }
-              else {  res.append('1'); carry=0;}            }
-            
-            if(a[i]=='1'&&b[j]=='1')
-            {
-                if(carry ==1)
-                {
-                    res.append('1');
-                    carry=1;
-                }
-               else { res.append('0');
-                carry=1;
-            }}
-            
-            i--;
-            j--;
+public class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() -1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
         }
-        while(i>=0)
-        {
-                if(a[i]=='1')
-                {
-                    if(carry==1)
-                    {
-                        res.append('0');
-                        carry=1;
-                    }
-                    else
-                    {
-                        res.append('1');
-                    }
-                    
-                }
-             if(a[i]=='0')
-                {
-                    if(carry==1)
-                    {
-                        res.append('1');
-                        carry=0;
-                    }
-                    else
-                    {
-                        res.append('0');
-                    }
-                    
-                }
-            
-            i--;
-        }
-        while(j>=0)
-        {
-                if(b[j]=='1')
-                {
-                    if(carry==1)
-                    {
-                        res.append('0');
-                        carry=1;
-                    }
-                    else
-                    {
-                        res.append('1');
-                    }
-                    
-                }
-             if(b[j]=='0')
-                {
-                    if(carry==1)
-                    {
-                        res.append('1');
-                        carry=0;
-                    }
-                    else
-                    {
-                        res.append('0');
-                    }
-                    
-                }
-            j--;
-            
-        }
-        if(carry==1)
-        {
-            res.append('1');
-        }
-        res.reverse();
-        
-        return res.toString();
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
     }
 }
