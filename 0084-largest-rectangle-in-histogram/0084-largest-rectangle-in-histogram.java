909fe47d95211c5e5aa512e20759596cb1387099
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-03-22 17:51:42 +0530

    Time: 276 ms (24.39%), Space: 58.5 MB (41.96%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        // Two Pass Solution 
+        
+        int n = heights.length;
+        int[] leftsmall = new int[n]; // for calculating left smaller elements =>boundary
+        int[] rightsmall = new int[n]; // calculating right small => Boundary
+        
+        Stack<Integer> st = new Stack();
+        // calculating left small
+        for(int i =0;i<n;i++)
+        {
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i])
+            {
+                st.pop();
+            }
+            if(st.isEmpty()) leftsmall[i]=0;
+            else leftsmall[i] =st.peek()+1;
+            st.push(i);
+            
+        }
+        // clear stack
+        while(!st.isEmpty())
+        {
+            st.pop();
+        }
+        // calculate right small
+        
+         for(int i=n-1;i>=0;i--)
+        {
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i])
+            {
+                st.pop();
+            }
+            if(st.isEmpty()) rightsmall[i]=n-1;
+            else rightsmall[i] =st.peek()-1;
+            st.push(i);
+            
+        }
+        
+        //calculate max height;
+        int ans =0;
+        for(int i=0;i<n;i++)
+        {
+            ans = Math.max(ans,((rightsmall[i]-leftsmall[i]+1)*heights[i]));
+        }
+        return ans;
+    }
+}
