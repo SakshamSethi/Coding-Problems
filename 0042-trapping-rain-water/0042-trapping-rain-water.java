f75892b41f9db5fd8f899a746279f24c44317c59
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-03-26 12:38:00 +0530

    Time: 1 ms (98.75%), Space: 43.5 MB (17.33%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int trap(int[] a) {
+        
+        // using two pointers approach 
+        
+        int l = 0;
+        int r = a.length-1;
+        int lmax=0;
+        int rmax=0;
+        int res=0;
+        
+        while(l<=r)
+        {
+            if(a[l]<=a[r])
+            {
+                if(a[l]>= lmax) { lmax= a[l] ;}
+                else {res += lmax-a[l] ; }
+            l++;
+            }
+            else 
+            {
+            if(a[r] >= rmax )
+            {
+                rmax = a[r];
+            }
+                else
+                {
+                    res+= rmax - a[r];
+                }
+                r--;
+            }
+            
+        }
+        return res;
+    }
+}
