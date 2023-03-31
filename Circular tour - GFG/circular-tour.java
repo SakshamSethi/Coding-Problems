f66e0e4728d24a7412b827ef28a9376df1a75d07
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-03-31 17:01:22 +0530

    Added solution - LeetHub

---
+++ Circular tour - GFG/circular-tour.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+class First_Circular_tour
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t =sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			sc.nextLine();
+			String str = sc.nextLine();
+			String arr[] = str.split(" ");
+			int p[] = new int[n];
+			int d[] = new int[n];
+			int j=0;
+			int k=0;
+			for(int i=0; i<2*n; i++)
+			{
+				if(i%2 == 0)
+				{
+					p[j]= Integer.parseInt(arr[i]);
+					j++;
+				}
+				else
+				{
+					d[k] = Integer.parseInt(arr[i]);
+					k++;
+				}
+			}
+			
+			System.out.println(new Solution().tour(p,d));
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+// In java function tour() takes two arguments array of petrol
+// and array of distance
+
+class Solution
+{
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(int petrol[], int distance[])
+    {
+	// Your code here
+	
+	int psum=0, dsum=0;
+	for(int x : petrol) psum+=x;
+	for(int x : distance) dsum+=x;
+	
+	if(dsum>psum) return -1;
+	
+	int fuel_storage=0;
+	int ans=0;
+	for(int i=0;i<petrol.length;i++)
+	{
+	    fuel_storage += petrol[i]-distance[i];
+	    
+	    if(fuel_storage<0)
+	    {
+	        fuel_storage = 0;
+	        ans=i+1;
+	    }
+	}
+	return ans;
+    }
+}
