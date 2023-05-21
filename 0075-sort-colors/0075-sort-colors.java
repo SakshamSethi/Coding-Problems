6307e7f23b5396e9605fc36301f4118d2414f924
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-05-21 14:38:09 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (54.75%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,7 +1,8 @@
 class Solution {
     public void sortColors(int[] arr) {
         
-        // Insertion Sort 
+        // Brute force - Insertion Sort or any other sorting
+        /*
          int n = arr.length;
         for (int i = 1; i < n; ++i) {
             int key = arr[i];
@@ -13,7 +14,27 @@ class Solution {
                 j = j - 1;
             }
             arr[j + 1] = key;
+        }*/
+        
+        // Better Approach
+        // using counters
+        
+        int c1 =0 ,c2=0,c3=0;
+
+        for(int x:arr)
+        {
+            if(x==0)c1++;
+            if(x==1)c2++;
+            if(x==2)c3++;
         }
+int i=0;
+        while(c1>0) 
+        {arr[i++] = 0;
+        c1--;
+        }
+         while(c2>0) {arr[i++] = 1; c2--;}
+        while(c3>0) {arr[i++] = 2; c3--;}
+    
     }
             
         }
