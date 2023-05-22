a0862e137618e3bdf5bfde31bb3a722d8b4275de
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-05-22 19:39:52 +0530

    Time: 1 ms (100.00%), Space: 59.7 MB (5.29%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i=0;i<nums.length;i++)
         {
             sum+= nums[i];
-            maxsum = Math.max(maxsum , sum);
+            if(maxsum<sum) maxsum=sum;
             if(sum<0) sum =0;
             
             
