e041c971e133bb80ab52b433f671ab7206f59166
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-05-28 18:36:23 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (70.92%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -16,7 +16,7 @@
     
     // DP solution : MEMOIZATION : Top-Down Approach
     
-  int[] dp = new int[31];
+  /*int[] dp = new int[31];
     
     public int fib(int n)
     {
@@ -29,6 +29,29 @@
         else 
             return dp[n]= fib(n-1)+fib(n-2);
     }
+    */
+    
+        // Using Tabulation
+        
+        public int fib(int n)
+        {
+            if(n==0)return 0;
+            if(n==1)return 1;
+            
+            int[] dp = new int[n+1];
+            
+            
+            Arrays.fill(dp,-1);
+            dp[0]=0;
+            dp[1]=1;
+            
+            for(int i=2; i<=n; i++)
+            {
+                dp[i]= dp[i-1]+dp[i-2];
+            }
+           
+            return dp[n];
+        }
 }
 
 
