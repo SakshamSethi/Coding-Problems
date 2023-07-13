c208e72e7675c7e364f183c34a7550acad541830
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-13 10:44:19 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (5.19%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -10,12 +10,34 @@ class Solution {
 Arrays.fill(arr,-1);
    }
     
-    return count(0,0,m-1,n-1,dp);
+    //return count(0,0,m-1,n-1,dp);
     
+    // TABULATION  
+      
+    dp[m-1][n-1] = 1;
+    
+    for(int i =m-1 ; i>=0;i--)
+    {
+      for(int j=n-1;j>=0;j--)
+      {
+       
+        if(i==m-1 && j==n-1) continue;
+        else
+        {
+         int down =0 , right =0;
+          if(i<m-1)   down = dp[i+1][j];
+         if(j<n-1) right = dp[i][j+1];
+          dp[i][j] = down+right;
+        }
+        
+    
+      }
+    }
+    return dp[0][0];
    
       
     }
-  int count(int a , int b , int m , int n ,int[][] dp )
+ /* int count(int a , int b , int m , int n ,int[][] dp )
   {
     if(a==m && b==n)
     {
@@ -30,5 +52,5 @@ Arrays.fill(arr,-1);
     int right = count(a,b+1,m,n,dp);
   
     return dp[a][b]=down+right;
-  }
+  }*/
 }
