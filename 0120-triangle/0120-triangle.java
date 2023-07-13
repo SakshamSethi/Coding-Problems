a5cfd17386f3428d0a16c1a102b625b765741e30
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-13 22:11:58 +0530

    Time: 5 ms (43.18%), Space: 44.2 MB (53.32%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -5,10 +5,30 @@ class Solution {
       if(n==1) return triangle.get(0).get(0);
       int[][]dp = new int[n][n];
       for(int[]arr : dp) Arrays.fill(arr,-1);
-      return count(triangle,0,0 ,n-1,dp );
+     // return count(triangle,0,0 ,n-1,dp );
+      
+      //Tabulation
+      for(int i=0;i<n;i++)
+      {
+        dp[n-1][i] = triangle.get(n-1).get(i);
+      }
+      
+      for(int i=n-2;i>=0;i--)
+      {
+        for(int j=i;j>=0;j--)
+        {
+          if(i==n-1&&j==n-1)continue;
+          
+          int d =  triangle.get(i).get(j)+ dp[i+1][j];
+          int dg =  triangle.get(i).get(j) + dp[i+1][j+1];
+          
+          dp[i][j]=Math.min(d,dg);
+        }
+      }
+      return dp[0][0];
       
     }
-  int count(List<List<Integer>> triangle , int a, int b,int i, int[][]dp )
+ /* int count(List<List<Integer>> triangle , int a, int b,int i, int[][]dp )
   {
     if(a==i) return triangle.get(a).get(b);
     
@@ -19,5 +39,5 @@ class Solution {
     
     return dp[a][b] = Math.min(d,dg);
    
-  }
+  }*/
 }
