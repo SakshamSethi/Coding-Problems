14408b320c8b0d7484051032894f454b7edd8bc4
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-13 20:54:58 +0530

    Time: 3 ms (30.90%), Space: 44 MB (58.45%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -10,11 +10,30 @@ class Solution {
         Arrays.fill(arr,-1);
       }
       
-      return count(grid ,m-1,n-1,dp );
+    //  return count(grid ,m-1,n-1,dp );
+      
+      //TABULATION :  
+      
+      //base case : 
+      dp[0][0] = grid[0][0];
+      
+      for(int i =0;i<m;i++)
+      {
+      for(int j =0 ; j<n;j++)
+      {
+        if(i==0&&j==0) continue;
+        int up=201 , left = 201;
+        
+        if(i>0) up = grid[i][j] + dp[i-1][j];
+        if(j>0) left = grid[i][j] + dp[i][j-1];
+        
+        dp[i][j] = Math.min(up,left);
+      }
       
     }
-  
-  int count(int[][] grid , int m , int n, int[][]dp )
+      return dp[m-1][n-1];
+    }
+  /*int count(int[][] grid , int m , int n, int[][]dp )
   {
     if(m==0 && n==0) return grid[0][0];
     
@@ -26,6 +45,6 @@ class Solution {
   int left = grid[m][n] + count(grid,m,n-1,dp);  
     return dp[m][n]= Math.min( left, up);
   
-  }
+  }*/
   
 }
