729e67bcc3f423fb8d81a0bd256d0865f3eed63a
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-13 11:29:05 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (50.46%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -7,15 +7,38 @@ class Solution {
       if(grid[m][n]==1) return 0;
       
       int[][] dp = new int[m+1][n+1];
-      for(int[]arr : dp)
+     // for(int[]arr : dp)
+    //  {
+    //    Arrays.fill(arr,-1);
+    //  }
+      
+      //return count(grid,0,0,m,n,dp);
+     
+      // Tabulation :
+      
+   dp[m][n]=1;
+      
+      for(int a =m ;a>=0;a--)
       {
-        Arrays.fill(arr,-1);
+        for(int b =n ; b>=0;b--)
+        {
+          
+          if(a==m && b==n) continue;
+        
+            if(grid[a][b]==1) { dp[a][b]=0; }
+          else
+          {
+            int down =0 , right=0;
+            if(a<m) down = dp[a+1][b];
+            if(b<n) right = dp[a][b+1];
+          dp[a][b]= down+right;
+          }
+          
+        }
       }
-      return count(grid,0,0,m,n,dp);
-      
-      
+      return dp[0][0];
     }
-  int count(int[][] grid , int a , int b , int m , int n,int[][]dp)
+  /*int count(int[][] grid , int a , int b , int m , int n,int[][]dp)
   {
     if(a==m && b==n)
       return 1;
@@ -27,5 +50,5 @@ class Solution {
     int right = count(grid,a,b+1,m,n,dp);
     
     return dp[a][b]= down+right;
-  }
+  }*/
 }
