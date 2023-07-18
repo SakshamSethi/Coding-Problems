013bd07a987bdbc455c367e82e259d62931e3019
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-18 20:24:44 +0530

    Time: 174 ms (40.04%), Space: 39.9 MB (97.90%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,28 +1,30 @@
 class Solution {
- public int count =0;
     public int findTargetSumWays(int[] nums, int target) {
-       if(nums.length==1 )
-       {if(nums[0]==target || nums[0]==target*-1) return 1;
-        return 0;
-       }
-     
-        check(nums,target,0,0);
-        return count;
-        
+
+      int n = nums.length;
+      int sum=0;
+      for(int x:nums) sum+=x;
+      
+      
+      int s2 = (sum-target);
+
+      if(s2%2!=0 || s2<0) return 0;
+      
+      return count(nums,s2/2,n-1 );
     }
-public void check(int nums[],int target,int sum,int i)
-{
-   
-    if(i == nums.length)
-    {
-        if(sum==target) {count++; return;}
-        
-        return;
-        
-    }
+  int count(int[]nums , int target , int n  )
+  {
     
-    check(nums,target,sum+nums[i],i+1);
-    check(nums,target,sum-nums[i],i+1);
-    return;
-}
+  if(n==0)
+			{
+				if(nums[n]==0 && target==0) return 2;
+				if(target==0 || target == nums[0]) return 1;
+				 return 0;
+			}
+    
+    int take = 0 ;
+    if(target>=nums[n]) take = count(nums,target-nums[n],n-1);
+    int notTake = count(nums,target,n-1);
+return take+notTake;
+  }
 }
