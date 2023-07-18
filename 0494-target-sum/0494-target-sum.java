ce7c41e20a537af653bad1a8fc45ec2794b842e4
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-18 20:29:12 +0530

    Time: 188 ms (39.48%), Space: 42.7 MB (55.47%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -12,7 +12,7 @@ class Solution {
       
       int[][]dp = new int[n][(s2/2)+1];
       
-      for(int[] arr: dp) Arrays.fill(arr,-1);
+      for(int[] arr: dp) Arrays.fill(arr,Integer.MIN_VALUE);
       
       return count(nums,s2/2,n-1,dp );
     }
@@ -25,7 +25,7 @@ class Solution {
 				if(target==0 || target == nums[0]) return 1;
 				 return 0;
 			}
-    if(dp[n][target]!=-1)return dp[n][target];
+    if(dp[n][target]!=Integer.MIN_VALUE)return dp[n][target];
     
     int take = 0 ;
     if(target>=nums[n]) take = count(nums,target-nums[n],n-1,dp);
