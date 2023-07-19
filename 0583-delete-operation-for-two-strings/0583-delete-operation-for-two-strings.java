53db355733f40620dda081a72bdc5e76159bde95
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-19 20:35:48 +0530

    Time: 10 ms (80.83%), Space: 43.4 MB (93.47%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -5,14 +5,29 @@ class Solution {
       int m = word2.length();
       
       int[][] dp = new int[n+1][m+1];
-      for(int[]arr : dp) Arrays.fill(arr,-1);
+     // for(int[]arr : dp) Arrays.fill(arr,-1);
       
-      int common = count(word1,word2,n,m,dp);
+    //  int common = count(word1,word2,n,m,dp);
+          for(int i=0;i<=n;i++) 
+      {
+        dp[i][0]=0;
+      }
+      for(int j=0;j<=m;j++)dp[0][j]=0;
       
-      return (n-common)+(m-common);
-      
+      for(int i= 1 ; i<=n ; i++)
+      {
+        for(int j=1;j<=m ; j++)
+        {
+          if(word1.charAt(i-1)==word2.charAt(j-1)) dp[i][j] = 1+dp[i-1][j-1];
+          else dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+        }
+      }
+      return (n-dp[n][m])+(m-dp[n][m]);
     }
-  int count(String w1 , String w2 , int n , int m , int[][]dp)
+     // return (n-common)+(m-common);
+      
+//}
+  /*int count(String w1 , String w2 , int n , int m , int[][]dp)
   {
     if(n==0||m==0 )return 0;
     if(dp[n][m]!=-1) return dp[n][m];
@@ -21,5 +36,5 @@ class Solution {
     
     else return dp[n][m] = Math.max(count(w1,w2,n-1,m,dp) , count(w1,w2,n,m-1,dp));
     
-  }
+  }*/
 }
