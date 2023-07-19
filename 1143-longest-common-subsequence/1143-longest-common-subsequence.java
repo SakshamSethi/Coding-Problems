9b6da4afdbba15f2cbc67c94a45a26ecf5b62838
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-19 09:38:03 +0530

    Time: 42 ms (25.71%), Space: 48.6 MB (30.57%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,18 +3,18 @@ class Solution {
       
       int i1 = s1.length();
       int i2 = s2.length();
-      int[][]dp = new int[i1][i2];
+      int[][]dp = new int[i1+1][i2+1];
       for(int arr[] : dp) Arrays.fill(arr,-1);
-      return count(s1,s2,i1-1,i2-1,dp);
+      return count(s1,s2,i1,i2,dp);
       
     }
   int count(String s1 , String s2 , int i1 , int i2,int[][]dp)
   {
-    if(i1<0 || i2<0)return 0;
+    if(i1==0 || i2==0)return 0;
     
     if(dp[i1][i2]!=-1)return dp[i1][i2];
     
-    if(s1.charAt(i1)==s2.charAt(i2)) return dp[i1][i2]=1 + count(s1,s2,i1-1,i2-1,dp);
+    if(s1.charAt(i1-1)==s2.charAt(i2-1)) return dp[i1][i2]=1 + count(s1,s2,i1-1,i2-1,dp);
     
     else return dp[i1][i2] = Math.max(count(s1,s2,i1-1,i2,dp),count(s1,s2,i1,i2-1,dp));
   }
