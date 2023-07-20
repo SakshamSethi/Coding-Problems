c8e8216e6927e005c137696dba6c3434527214f0
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-20 11:59:34 +0530

    Time: 7 ms (36.70%), Space: 43.9 MB (37.44%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -5,10 +5,28 @@ class Solution {
       int m = w2.length();
       int[][]dp = new int[n+1][m+1];
       for(int arr[] : dp) Arrays.fill(arr,-1);
-      return count(w1,w2,n,m,dp);
+    //  return count(w1,w2,n,m,dp);
+    
+  for(int i=0 ; i<=n ; i++) dp[i][0]=i;
+      for(int j=0 ; j<=m ; j++) dp[0][j]=j;
+    
+      for(int i=1 ; i<=n ;i++)
+      {
+        
+        for(int j=1 ; j<=m ; j++)
+        {
+          if(w1.charAt(i-1)==w2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+          else
+          {
+            dp[i][j] = Math.min(1+dp[i][j-1] , Math.min(1+dp[i-1][j] , 1+dp[i-1][j-1]));
+          }
+        }
+        
+      }
+      return dp[n][m];
     }
   
-  int count(String w1 , String w2 , int n , int m , int[][]dp)
+  /*int count(String w1 , String w2 , int n , int m , int[][]dp)
   {
     
     if(n==0) return m;
@@ -19,7 +37,7 @@ class Solution {
     
     else return dp[n][m] = Math.min(1+count(w1,w2,n,m-1,dp) , Math.min(1+count(w1,w2,n-1,m,dp),1+count(w1,w2,n-1,m-1,dp)));
     
-  }
+  }*/
   
   
 }
