750be7f0e2b3bc6c579a3a7d69bedaa799d757e5
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-20 13:17:47 +0530

    Time: 40 ms (40.96%), Space: 44.7 MB (73.92%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -3,12 +3,40 @@ class Solution {
         
       int n = s.length();
       int m = p.length();
-      int[][]dp = new int[n+1][m+1];
-      for(int[]arr:dp)Arrays.fill(arr,-1);
+    //  int[][]dp = new int[n+1][m+1];
+     // for(int[]arr:dp)Arrays.fill(arr,-1);
       
-      return match(s,p,n,m,dp);
+     // return match(s,p,n,m,dp);
+      
+      boolean[][]dp = new boolean[n+1][m+1];
+      dp[0][0]=true;
+      
+      for(int i =1 ; i<=n; i++) dp[i][0]=false;
+     
+ 
+         for (int i = 1; i <= m; i++) {
+        if (p.charAt(i - 1) == '*') {
+            dp[0][i] = dp[0][i - 1];
+        }
     }
-  boolean match(String s , String p , int n , int m,int[][]dp)
+    
+  
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=1;j<=m;j++)
+    {
+      
+      if(p.charAt(j-1)=='?'||s.charAt(i-1)==p.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+      else if(p.charAt(j-1)=='*')
+      {
+        dp[i][j] = (dp[i-1][j] || dp[i][j-1]);
+      }
+      else dp[i][j]=false;
+    }
+  }
+  return dp[n][m]; 
+      }
+  /*boolean match(String s , String p , int n , int m,int[][]dp)
   {
     if(n==0 && m==0) return true ;
     
@@ -46,5 +74,5 @@ class Solution {
       dp[n][m]=0;
       return false ;
     }
-  }
+  }*/
 }
