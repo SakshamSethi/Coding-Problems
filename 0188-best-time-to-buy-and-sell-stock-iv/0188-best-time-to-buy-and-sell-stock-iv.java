3fa050e9182ea6baf8bbf0474490cb170ca4b9d2
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-21 09:50:28 +0530

    Time: 2 ms (88.11%), Space: 40.3 MB (93.43%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
           int n = prices.length;
-      int[][][]dp = new int[n][2][k+1];
+      /*int[][][]dp = new int[n][2][k+1];
       
       for(int arr[][] : dp )
       {
@@ -10,8 +10,39 @@ class Solution {
       }
       
       return count(prices,0,1,k,dp);
+    */
+      
+      
+      // Direct most optimised version :
+       //iska space optimize 
+      int[]ahead = new int [2*(k+1)];
+            int[]cur = new int [2*(k+1)];
+
+      for(int i=n-1;i>=0;i--)
+      {
+        for(int trans=(2*(k+1))-2; trans>=0;trans--)
+        {
+            int profit =0;
+    
+    if(trans%2==0)
+    {
+      profit = Math.max(-prices[i]+ahead[trans+1],ahead[trans]);
     }
-    int count(int[]prices,int i , int buy , int cap,int[][][]dp)
+    else
+    {
+      profit=Math.max(prices[i]+ahead[trans+1],ahead[trans]);
+    }
+     cur[trans]=profit;
+        }
+        ahead=cur;
+      }
+    return ahead[0];
+      
+    }
+      
+      
+      
+    /*int count(int[]prices,int i , int buy , int cap,int[][][]dp)
   {
     
     if(cap==0)return 0 ;
@@ -34,5 +65,5 @@ class Solution {
       
       
     
-  }
+  }*/
 }
