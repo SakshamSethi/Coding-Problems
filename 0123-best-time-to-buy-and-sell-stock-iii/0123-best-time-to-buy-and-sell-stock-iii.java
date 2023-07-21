15fbb1fe4418ad0a49ab03db4fb51326bb6a0986
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-21 09:14:29 +0530

    Time: 82 ms (37.49%), Space: 63.1 MB (29.80%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -2,18 +2,57 @@ class Solution {
     public int maxProfit(int[] prices) {
       
       int n = prices.length;
-      int[][][]dp = new int[n][2][3];
+      /*int[][][]dp = new int[n][2][3];
       
       for(int arr[][] : dp )
       {
         for(int[]a : arr)
           Arrays.fill(a,-1);
       }
+      */
+     // return count(prices,0,1,2,dp);
       
-      return count(prices,0,1,2,dp);
+      // Tabulation :
+      int[][][]dp = new int[n+1][2][3];
+      for(int i=0 ; i<=n;i++)
+      {
+        for(int j =0 ; j<2;j++)
+        {
+          dp[i][j][0]=0;
+        }
+      }
+      
+      for(int i=0;i<2;i++)
+      {
+        for(int j=0;j<3;j++)
+        {
+          dp[n][i][j]=0;
+        }
+      }
+      
+      for(int i=n-1;i>=0;i--)
+      {
+        for(int buy=0;buy<2;buy++)
+        {
+          for(int cap=1;cap<3;cap++)
+          {
+             int profit = 0;
+           if(buy==1)
+        {
+      profit = Math.max(-prices[i]+dp[i+1][0][cap], dp[i+1][1][cap] );
       
     }
-  int count(int[]prices,int i , int buy , int cap,int[][][]dp)
+    else
+    {
+      profit= Math.max(prices[i]+dp[i+1][1][cap-1] ,dp[i+1][0][cap]);
+    }
+       dp[i][buy][cap]=profit;
+          }
+        }
+      }
+      return dp[0][1][2];
+    }
+ /* int count(int[]prices,int i , int buy , int cap,int[][][]dp)
   {
     
     if(cap==0)return 0 ;
@@ -36,5 +75,5 @@ class Solution {
       
       
     
-  }
+  }*/
 }
