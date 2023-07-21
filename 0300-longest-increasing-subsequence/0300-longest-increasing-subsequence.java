e96af9dab8e4b5ab6f0f342c313436b26603e172
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-21 14:31:30 +0530

    Time: 151 ms (19.09%), Space: 94.5 MB (18.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -5,12 +5,32 @@ class Solution {
       
       int[][]dp = new int[n+1][n+1];
       
-      for(int[]arr:dp)Arrays.fill(arr,-1);
+      //for(int[]arr:dp)Arrays.fill(arr,-1);
       
-      return count(nums,1,0,dp);
+    //  return count(nums,1,0,dp);
       
+      //Tabulation :
+      
+      for(int i = n-1 ; i>=0 ; i--)
+      {
+        for(int prev = i-1 ; prev>=-1;prev--)
+        {
+           int notTake = dp[i+1][prev+1];
+    
+    int take =0;
+    
+    if(prev==-1|| nums[i]>nums[prev])
+    {
+      take = 1+dp[i+1][i+1];
     }
-  int count(int[]nums, int i , int prev,int[][]dp)
+ dp[i][prev+1] = Math.max(take,notTake);
+          
+        }
+        
+      }
+      return dp[0][0];
+    }
+  /*int count(int[]nums, int i , int prev,int[][]dp)
   {
     if(i==nums.length+1)return 0;
     
@@ -25,5 +45,5 @@ class Solution {
       take = 1+count(nums,i+1,i,dp);
     }
     return dp[i][prev] = Math.max(take,notTake);
-  }
+  }*/
 }
