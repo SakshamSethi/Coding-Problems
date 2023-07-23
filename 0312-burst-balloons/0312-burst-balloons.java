e121dbe62b445dc599300e9f0b48b9753e863828
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-23 22:09:30 +0530

    Time: 125 ms (55.92%), Space: 41.3 MB (86.62%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxCoins(int[] nums) {
       int n=nums.length;  
       int arr[] = new int[n+2];
-      int[][]dp = new int[n+2][n+2];
+      int[][]dp = new int[n+1][n+1];
       for(int[]a:dp)Arrays.fill(a,-1);
       for(int i=0;i<n;i++)
       {
