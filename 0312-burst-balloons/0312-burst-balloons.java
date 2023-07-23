4aed07a2ad3fa5fa16d0dc7236a34f636439d59e
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-23 22:16:18 +0530

    Time: 38 ms (96.34%), Space: 41.8 MB (43.38%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -2,17 +2,39 @@ class Solution {
     public int maxCoins(int[] nums) {
       int n=nums.length;  
       int arr[] = new int[n+2];
-      int[][]dp = new int[n+1][n+1];
-      for(int[]a:dp)Arrays.fill(a,-1);
+      int[][]dp = new int[n+2][n+2];
+      //for(int[]a:dp)Arrays.fill(a,-1);
       for(int i=0;i<n;i++)
       {
         arr[i+1]=nums[i];
       }
       arr[0]=1;
       arr[n+1]=1;
-      return f(arr , 1 , n,dp);
+     // return f(arr , 1 , n,dp);
+    
+      //tabulation :
+      
+      for(int i = n ; i>=1 ;i--)
+      {
+        for(int j=1 ; j<=n ; j++)
+        {
+          if(i>j)continue;
+          
+        int maxi = Integer.MIN_VALUE;
+    
+    for(int k = i ; k<=j ; k++)
+    {
+      int cost = (arr[i- 1] * arr[k] * arr[j+ 1]) + dp[i][k-1] + dp[k+1][j];
+       maxi = Math.max(maxi,cost);
     }
-  int f(int[]nums,int i , int j ,int[][]dp )
+    dp[i][j]= maxi;
+          
+          
+        }
+      }
+    return dp[1][n];
+    }
+  /*int f(int[]nums,int i , int j ,int[][]dp )
   {
     if(i>j)return 0;
     if(dp[i][j]!=-1)return dp[i][j];
@@ -24,5 +46,5 @@ class Solution {
        maxi = Math.max(maxi,cost);
     }
     return dp[i][j]= maxi;
-  }
+  }*/
 }
