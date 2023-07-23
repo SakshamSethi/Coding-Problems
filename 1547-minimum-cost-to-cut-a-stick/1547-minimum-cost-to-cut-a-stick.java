5e17d4785b984bc1a484fb5c53f231cd88853292
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-23 20:02:29 +0530

    Time: 11 ms (84.68%), Space: 41.5 MB (65.29%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -11,11 +11,34 @@ class Solution {
         arr[i+1]=cuts[i];
       }
   int[][]dp = new int[len+2][len+2];
-      for(int[] ar : dp) Arrays.fill(ar,-1);
-      return f(1,len,arr,dp);
+   //   for(int[] ar : dp) Arrays.fill(ar,-1);
+    //  return f(1,len,arr,dp);
     
+      //Tabulation :
+      
+     
+      
+      for(int i=len ; i>=1;i--)
+      {
+        for(int j=1 ;j<=len;j++)
+        { 
+          if(i>j)continue;
+          int mini = Integer.MAX_VALUE;
+          for(int ind = i ; ind<=j;ind++)
+          {
+           
+            int cost = (arr[j+1]-arr[i-1]) + dp[i][ind-1] + dp[ind+1][j];
+            mini = Math.min(mini,cost);
+          }
+          dp[i][j]=mini;
+        }
+        
+      }
+       
+      return dp[1][len];
+      
     }
-  int f(int i , int j , int[]arr,int[][]dp)
+  /*int f(int i , int j , int[]arr,int[][]dp)
   {
     
     if(i>j)return 0;
@@ -30,5 +53,5 @@ class Solution {
     mini = Math.min(mini,cost);
     }
     return dp[i][j]=mini;
-  }
+  }*/
 }
