be8b1aee453860a9139f748587754cc518f7db1f
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-23 22:07:55 +0530

    Time: 125 ms (55.92%), Space: 41.9 MB (22.82%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,32 +1,28 @@
 class Solution {
-   public int maxCoins(int[] nums) {
-    int[][] dp = new int[nums.length][nums.length];
-    return maxCoins(nums, 0, nums.length - 1, dp);
-}
-
-public int maxCoins(int[] nums, int start, int end, int[][] dp) {
-    if (start > end) {
-        return 0;
+    public int maxCoins(int[] nums) {
+      int n=nums.length;  
+      int arr[] = new int[n+2];
+      int[][]dp = new int[n+2][n+2];
+      for(int[]a:dp)Arrays.fill(a,-1);
+      for(int i=0;i<n;i++)
+      {
+        arr[i+1]=nums[i];
+      }
+      arr[0]=1;
+      arr[n+1]=1;
+      return f(arr , 1 , n,dp);
     }
-    if (dp[start][end] != 0) {
-        return dp[start][end];
+  int f(int[]nums,int i , int j ,int[][]dp )
+  {
+    if(i>j)return 0;
+    if(dp[i][j]!=-1)return dp[i][j];
+    int maxi = Integer.MIN_VALUE;
+    
+    for(int k = i ; k<=j ; k++)
+    {
+      int cost = (nums[i- 1] * nums[k] * nums[j+ 1]) + f(nums,i,k-1,dp) +f(nums,k+1,j,dp);
+       maxi = Math.max(maxi,cost);
     }
-    int max = nums[start];
-    for (int i = start; i <= end; i++) {
-        int val = maxCoins(nums, start, i - 1, dp) + 
-                  get(nums, i) * get(nums, start - 1) * get(nums, end + 1) + 
-                  maxCoins(nums, i + 1, end, dp);
-                  
-        max = Math.max(max, val);
-    }
-    dp[start][end] = max;
-    return max;
-}
-
-public int get(int[] nums, int i) {
-    if (i == -1 || i == nums.length) {
-        return 1;
-    }
-    return nums[i];
-}
+    return dp[i][j]= maxi;
+  }
 }
