8550487510711d3d396e0e920fd75158483381e1
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-25 23:37:51 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    class Pair
+    {
+        int node , parent ;
+        
+        Pair(int node , int parent)
+        {
+            this.node = node ;
+            this.parent = parent ;
+        }
+    }
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+
+        boolean[]visited = new boolean[V];
+        
+        for(int i=0 ; i<V;i++)
+        {
+            if(!visited[i])
+            {
+                if(cycle(i,visited,adj)) return true;
+            }
+        }
+        
+        return false;
+    }
+    boolean cycle(int i , boolean[]visited , ArrayList<ArrayList<Integer>>adj)
+    {
+        visited[i]=true;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i , -1));
+        
+        while(!q.isEmpty())
+        {
+            int node = q.peek().node ;
+            int parent = q.peek().parent;
+            q.remove();
+            
+            for(int adjNode : adj.get(node))
+            {
+                if(!visited[adjNode])
+                {
+                    visited[adjNode]=true;
+                    q.add(new Pair(adjNode,node));
+                }
+                else 
+                {
+                    if(parent!=adjNode)return true;
+                }
+            }
+        }
+        return false;
+    }
+}
