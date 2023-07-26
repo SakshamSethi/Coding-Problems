191e990e28dd7b0a50296f5cb26910ef0b648c14
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-26 20:08:04 +0530

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        
+        int[]visited = new int[V];
+        int[]path = new int[V];
+        
+        for(int i =0 ; i<V ; i++)
+        {
+            if(visited[i]==0) 
+            {
+                if(dfs(i,visited,path,adj)==true)return true;
+            }
+        }
+        return false;
+        
+    }
+    boolean dfs(int node , int[]visited , int[]path ,ArrayList<ArrayList<Integer>> adj )
+    {
+        visited[node]=1;
+        path[node]=1;
+        
+        for(int u : adj.get(node))
+        {
+            if(visited[u]==0)
+            {
+                if(dfs(u,visited,path,adj)==true)return true;
+            }
+            else if(path[u]==1)
+            {
+                return true;
+            }
+        }
+        path[node]=0;
+        return false;
+    }
+}
