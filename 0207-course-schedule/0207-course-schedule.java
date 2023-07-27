2c38171d8994cda5a3ecfc895371782ae42c402f
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-07-27 09:53:00 +0530

    Time: 7 ms (31.43%), Space: 44.7 MB (14.52%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean canFinish(int V, int[][] prerequisites) {
+      
+      List<ArrayList<Integer>>adj = new ArrayList();
+      
+      for(int i = 0;i <V ; i++)
+        adj.add(new ArrayList<Integer>());
+      
+      int m = prerequisites.length;
+      
+      for(int i=0;i<m;i++)
+      {
+        adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        
+        }
+              int[]indegree = new int[V];
+        
+        for(int i=0 ; i<V ;i++)
+        {
+            for(int u : adj.get(i))
+            {
+                indegree[u]++;
+            }
+        }
+        
+        Queue<Integer>q = new LinkedList<>();
+        
+        for(int i =0 ; i<V ;i++)
+        {
+            if(indegree[i]==0)
+            q.offer(i);
+        }
+      
+       // int[]toposort = new int[V];
+        int k=0;
+        while(!q.isEmpty())
+        {
+            int node = q.remove();
+            //toposort[k++]=node;
+          k++;
+            for(int u : adj.get(node))
+            {
+                indegree[u]--;
+                if(indegree[u]==0) q.add(u);
+            }
+        }
+    
+       if(k==V)return true;
+      return false;
+
+      
+    }
+}
