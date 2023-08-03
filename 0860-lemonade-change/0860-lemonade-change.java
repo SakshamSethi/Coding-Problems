8f275f4637379afc28391ea9893ffac9df500807
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-03 21:09:03 +0530

    Time: 2 ms (81.45%), Space: 56.2 MB (95.04%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,5 +1,61 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
+      
+  
+      int fiveDollor=0 , tenDollor=0;
+      for(int i=0 ; i<bills.length ; i++)
+      {
+        
+        
+        if(bills[i]==5)
+        {
+            fiveDollor++;
+        }
+        
+        
+        else  if(bills[i]==10)
+            {
+          if(fiveDollor<=0)return false;
+             
+
+        else
+        {   fiveDollor--; 
+            tenDollor++;
+           
+        }   
+            
+        }
+        
+          else
+          {
+            if(tenDollor>0 && fiveDollor>0) // koshish krenge ki jada se jada 10 k note return krne ki 
+            {
+              tenDollor--;
+              fiveDollor--;
+            }
+         else   if(fiveDollor >= 3)
+            {
+              fiveDollor-=3;
+            }
+           
+            else
+            {
+              return false;
+            }
+          }
+          
+      }
+      
+      
+      return true;
+        
+    }
+  
+  
+}
+
+/*class Solution {
+    public boolean lemonadeChange(int[] bills) {
         int count5 = 0;
         int count10 = 0;
         for(int b : bills){
@@ -27,4 +83,4 @@ class Solution {
         }
         return true;
     }
-}
+}*/
