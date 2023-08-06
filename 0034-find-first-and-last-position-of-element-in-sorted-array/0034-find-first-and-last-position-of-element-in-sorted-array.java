cbd76cafae4f36e69c0080df569569d7346ac0d8
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-06 10:20:49 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (98.42%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,47 +1,67 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[2];
-        ans[0]=ans[1]=-1;
-int s = 0;
-        int m =0;
-        int e =  nums.length-1;
-        while(s<=e)
-        {
-            m = s + (e-s)/2;
-            if(target==nums[m])
-            {
-                ans[0]=m;
-                e = m-1;
-            }
-            else if(target>nums[m])
-            {
-                s = m+1;
-            }
-            else
-            {
-                e = m-1;
-            }
-        }
-         s=0;
-        e=nums.length-1;
         
-      while(s<=e)
-        {
-            m = s + (e-s)/2;
-            if(target==nums[m])
-            {
-                ans[1]=m;
-                s = m+1;
-            }
-            else if(target>nums[m])
-            {
-                s = m+1;
-            }
-            else
-            {
-                e = m-1;
-            }
-        }
-        return ans;
+      
+      int i = binarySearchStart(nums,target);
+     
+        int j = binarySearch(nums,target,0);
+      
+      return new int[]{i,j};
     }
+  
+  int binarySearch(int[]nums,int target , int start)
+  {
+    int end = nums.length -1 ;
+     int ans=-1;    
+    while(start<=end)
+    {
+      int m = start + (end-start)/2;
+
+      if(nums[m]== target)
+      {
+        ans = m ;
+        start = m+1;
+      }
+      else if(nums[m]<target)
+      {
+        start=m+1;
+      }
+      else
+      {
+        end = m-1;
+      }
+    }
+    return ans;
+  }
+  
+  int binarySearchStart(int[]nums , int target)
+  {
+    int start = 0 ;
+    int end = nums.length-1;
+    int ans = -1;
+    while(start<=end)
+    {
+      
+      int m = start + (end-start)/2;
+      
+      if(nums[m]==target)
+      {
+        ans = m ; 
+        end = m-1;
+      }
+      else if(nums[m]<target)
+      {
+        start=m+1;
+      }
+      else
+      {
+        end = m-1;
+      }
+    }
+    return ans;
+    
+  }
+  
+  
+  
 }
