614274b94d06719734c4f464c89aafe6b9f92618
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-06 11:31:37 +0530

    Time: 1 ms (99.85%), Space: 48.1 MB (67.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,20 +3,18 @@ class Solution {
       
       // HashMap solution 
       
-      TreeMap<Integer,Integer> map = new TreeMap<>();
+   /*   TreeMap<Integer,Integer> map = new TreeMap<>();
       int ans=-1;
       for(int i=0 ; i<nums.length ; i++)
       {
-        if(!map.containsKey(nums[i]))
+        if(map.containsKey(nums[i]))
         {
-           map.put(nums[i],1);
-      
+          
+          map.put(nums[i] , map.get(nums[i])+1);
         }
     
-        else {
-               map.put(nums[i] , map.get(nums[i])+1);
-        }
-        
+        else {  map.put(nums[i],0);
+             }
         if(map.get(nums[i])>nums.length/2)
         {
           ans = nums[i];
@@ -24,7 +22,24 @@ class Solution {
         }
       }
 
-      return ans;
+      return ans; */
+      
+      // Moore's Voting algo 
+      
+      int count =1 ;
+      int ele = nums[0];
+      int n = nums.length;
+      
+      for(int i=1 ; i<n ; i++)
+      {
+        if(count==0) ele=nums[i];
+        
+        if(ele == nums[i]) count++;
+        else count --;
+        
+        
+      }
+      return ele;
       
     }
 }
