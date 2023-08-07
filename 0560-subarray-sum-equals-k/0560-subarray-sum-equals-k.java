0c41a801dc8f453bbc90c813dbdc628d82dda610
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-07 19:09:30 +0530

    Time: 1090 ms (27.56%), Space: 44.8 MB (93.37%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,43 +1,26 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        
-        
-        // This Approach Only works for +ve numbers
-      /*  int max =0;
-        int sum=nums[0];
-int n = nums.length;        int j=0;int i=0;
-        while( j<n)
+      
+      int n = nums.length;
+      int count = 0 ;
+      for(int i=0 ; i< n ; i++)
+      {
+        int sum=nums[i];
+        if(sum == k)
         {
-       while(i<j && sum>k)
-       {
-           sum-=nums[i];
-           i++;
-       }
-            
-            if(sum==k) max++;
-            
-        j++;
-       if(j<n) sum+=nums[j];      
+          count++;
+        
         }
-        return max;
-    */
-        // Have to use prefix sum property inlcuding a hash map for solving this problem having -ve integers
-        
-        Map<Integer,Integer> ps = new HashMap<>(); //sum and frequency of sum that how many times that sum has occured during the traversal 
-        
-        int count =0;
-        int sum=0;
-        ps.put(0,1); // for initial encounter 
-        for(int i=0;i<nums.length;i++)
+        for(int j=i+1 ; j<n ; j++)
         {
-            sum+=nums[i];
-            
-            
-            if(ps.containsKey(sum-k))
-                count += ps.get(sum-k);
-            ps.put(sum, ps.getOrDefault(sum,0)+1);
-            
+          sum+=nums[j];
+          if(sum==k)
+          {count++;
+          
+          }
         }
-        return count;
+      }
+       return count ; 
+      
     }
 }
