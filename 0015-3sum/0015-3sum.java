4a65b70951ec4b5ef8249269553b21df79644182
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-08 12:06:10 +0530

    Time: 1508 ms (5.00%), Space: 52.1 MB (10.37%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+     //HashSet solution :
+      
+      int n = nums.length ;
+   // a[i]+a[j]+a[k]---> store a[j] : check if a[k] = a[i]-a[j] exists in the set --> one triplet
+      
+
+
+      HashSet<List<Integer>> set = new HashSet<>();
+            
+      for(int i=0 ; i<n ; i++)
+      {
+        HashSet<Integer> st = new HashSet<>();
+        
+        for(int j=i+1 ; j<n ; j++)
+        {
+          int third = -(nums[i]+nums[j]);
+          
+          if(st.contains(third))
+          {
+            List<Integer> temp = Arrays.asList(nums[i],nums[j],third);
+            
+            Collections.sort(temp);
+            set.add(temp);
+            
+          }
+          
+          
+          st.add(nums[j]);
+        }
+       
+      }
+      
+            List<List<Integer>> ans = new ArrayList<>(set);
+            return ans;
+      
+    }
+}
