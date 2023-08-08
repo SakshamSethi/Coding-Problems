8e9636e8fc02fde56c09bdf677d1ad2d828a18dd
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-08 09:40:34 +0530

    Time: 2 ms (98.28%), Space: 47 MB (30.24%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
       
-      HashMap<Integer,Integer> map = new HashMap<>();
+    /*  HashMap<Integer,Integer> map = new HashMap<>();
       int n=nums.length;
       for(int num : nums)
       {
@@ -13,5 +13,59 @@ class Solution {
         if(e.getValue()>n/3) ans.add(e.getKey());
       }
       return ans;
-    }
+    */
+      
+      // Moore's Voting Algorithm 
+      
+      int n = nums.length ;
+      
+      int cnt1 = 0;
+      int cnt2 =0 ;
+      
+      int ele1 = Integer.MIN_VALUE;
+      int ele2 = Integer.MIN_VALUE;
+      
+      for(int i=0 ; i<n ; i++)
+      {
+        if(cnt1==0 && nums[i]!=ele2)
+        {
+          cnt1 = 1;
+          ele1 = nums[i];
+        }
+        
+        else if(cnt2==0 && nums[i]!=ele1)
+        {
+          cnt2=1;
+          ele2 = nums[i];
+        }
+        
+        else if(ele1==nums[i]) cnt1++;
+        else if(ele2==nums[i]) cnt2++;
+        
+        else
+        {
+          cnt1--;
+          cnt2--;
+        }
+        
+        
+      }
+      
+      cnt1 = 0;
+      cnt2 = 0;
+      
+      for(int num : nums)
+      {
+        if(num==ele1)cnt1++;
+        else if(num==ele2)cnt2++;
+      }
+      
+List<Integer> ans = new ArrayList<>();
+      
+      if(cnt1>(n/3))ans.add(ele1);
+      if(cnt2>(n/3))ans.add(ele2);
+      return ans;
+        
+      
+    }   
 }
