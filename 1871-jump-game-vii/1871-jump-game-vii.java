7a443bfb52493670b2ca561b3ad984ea51cc0b59
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-09 19:10:54 +0530

    Time: 12 ms (75.55%), Space: 54.2 MB (29.26%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.java
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -1,28 +1,35 @@
 class Solution {
     public boolean canReach(String s, int minJump, int maxJump) {
-     
+        
       Queue<Integer> q = new LinkedList<>();
       
-      q.offer(0);
-      for(int i= 1 ; i<s.length();i++)
+      //--> check if the current element is 0 or not --> if it is zero , we will check that if it is possibly  reachable throught the previous 0's which we will be storing inside out queue (they should be in range of the previous zeroes )
+    
+      // so if at any point the queue becomes empty which we have reach a 0 we can say that zero is not possible to reach and hence we conclude that there is no other way to reach the end and we return false
+    
+      
+      int n=s.length();
+      if(s.charAt(n-1)!='0')return false ;
+      q.offer(0); // store index 0 as the start point 
+      
+      for(int i=1 ; i<n ; i++)
       {
         if(s.charAt(i)=='0')
         {
-          while(!q.isEmpty() && q.peek()<i-maxJump) q.poll();
+         while(!q.isEmpty() && q.peek()+maxJump<i) q.poll();
           
-         if(q.isEmpty()) return false;
-          
+          if(q.isEmpty()) return false;
+        
           int prev = q.peek();
-          
-          if(prev+minJump<=i && prev+maxJump>=i)
+          if(prev+minJump<=i && prev+maxJump>=i )
           {
+            if(i==n-1)return true;
             q.offer(i);
-            if(i==s.length()-1)return true;
+            
           }
-          
+            
         }
       }
-      
       return false;
     }
 }
