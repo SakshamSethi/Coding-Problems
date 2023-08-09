30b7697631be19ce4f7392feea2583e2691c1916
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-09 10:36:13 +0530

    Time: 6 ms (90.79%), Space: 44 MB (98.69%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,34 +1,33 @@
 class Solution {
     public boolean validPalindrome(String s) {
         
-        int i=0;
-        int j=s.length()-1; int flag=1;
-        while(i<j)
+      int i = 0;
+      int j = s.length()-1;
+      
+      while(i<j)
+      {
+        if(s.charAt(i)==s.charAt(j))
         {
-            if(s.charAt(i)==s.charAt(j))
-            {
-                i++;
-                j--;
-            }
-            if(s.charAt(i)!=s.charAt(j))
-            {
-               return Pal(s,i+1,j) || Pal(s,i,j-1);
-            }
-            
+          i++;
+          j--;
         }
-        return true;
+        else
+        {
+          return isValid(s,i+1,j) || isValid(s,i,j-1);
+        }
+      }
+      return true;
         
     }
-    boolean Pal(String s , int i, int j)
+  
+  boolean isValid(String s , int i , int j)
+  {
+    while(i<j)
     {
-        while(i<j)
-        {
-            if(s.charAt(i)!=s.charAt(j))
-                return false;
-            
-            i++; j--;
-        }
-        return true;
+      if(s.charAt(i)!=s.charAt(j)) return false;
+      
+      i++ ; j--;
     }
-    
+    return true;
+  }
 }
