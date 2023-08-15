7aa1b3a6d8623d067dfc9a6a24dfd02f0874f13b
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-15 13:46:19 +0530

    Time: 1 ms (100.00%), Space: 39.9 MB (40.36%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,11 +1,31 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int i =1;
-        while(n>=i)
+     // Can use Binary Search as well using formula of 1+2+...+k = k(k+1)/2 <=N 
+      
+      long start = 1 , end = n , k = 0 , curr = 0;
+      long ans = 0;
+      while(start<=end)
+      {
+        k = start + (end - start)/2;
+        
+        // use the formulae to the the total sum of elements till K th row 
+        // if the total sum of k elements equals the N then it means all rows have been filled or else                we check accordingly !
+        
+        // last row may remail incomplete , we will be using ceil value to return no of rows filled !
+        
+        curr = k*(k+1)/2;
+        
+        if(curr<=n)
         {
-            n=n-i;
-            i++;
+          ans = k ;
+          start = k+1;
         }
-        return i-1;
+        else
+        {
+          end=k-1;
+        }
+        
+      }
+      return (int)ans;
     }
 }
