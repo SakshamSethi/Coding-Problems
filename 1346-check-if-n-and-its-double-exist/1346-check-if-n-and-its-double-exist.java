c4bed11188f06e3c36810b6e8b31cf2280630018
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-17 10:19:38 +0530

    Time: 1 ms (98.86%), Space: 43.2 MB (12.82%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,20 +1,27 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-      int n =arr.length;
-        
-     Set<Integer> s = new HashSet<>();
-       
-        
-        for(int x:arr)
-        {
-            if(s.contains(x*2) || ((x%2==0)&&s.contains(x/2)) )
-                return true;
-            s.add(x);
-            
-            
-        }
-        
-        return false;
-        
+     
+  
+      // can sort the array and perform binary search for each element to find out if 
+      // there exists its double in the array or not 
+      // nlogn complexity 
+      
+      // can use a set to check if it contains the double of an element 
+      // will use  O(n) time but o(n) space as well
+      
+      
+      HashSet<Integer>hs = new HashSet();
+      
+  
+      
+      for(int ele : arr)
+      { 
+        if(hs.contains(ele*2)||(ele%2==0 && hs.contains(ele/2))) return true;
+     
+        hs.add(ele);
+          
+      }
+      return false;
 }
+  
 }
