55ee64b74cf099ecbf4fda5b5e6eb03f84005203
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-17 10:54:43 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (86.19%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,67 +1,48 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+  
+      
+      int start = 0;
+      int end = nums.length-1;
+      
+      int first=-1;
+      int second = -1;
+      
+      
+      while(start<=end)
+      {
+        int mid = start + (end-start)/2;
         
+        if(nums[mid]==target)
+        {
+          first = mid ;
+          end = mid-1;
+        }
+        else if(nums[mid]>target) end = mid-1;
+        else start = mid+1;
+          
+      }
       
-      int i = binarySearchStart(nums,target);
-     
-        int j = binarySearch(nums,target,0);
       
-      return new int[]{i,j};
+      start = 0;
+      end = nums.length-1;
+      
+       while(start<=end)
+      {
+        int mid = start + (end-start)/2;
+        
+        if(nums[mid]==target)
+        {
+          second = mid ;
+          start = mid+1;
+        }
+        else if(nums[mid]>target) end = mid-1;
+        else start = mid+1;
+          
+      }
+      
+      return new int[]{first,second};
     }
   
-  int binarySearch(int[]nums,int target , int start)
-  {
-    int end = nums.length -1 ;
-     int ans=-1;    
-    while(start<=end)
-    {
-      int m = start + (end-start)/2;
-
-      if(nums[m]== target)
-      {
-        ans = m ;
-        start = m+1;
-      }
-      else if(nums[m]<target)
-      {
-        start=m+1;
-      }
-      else
-      {
-        end = m-1;
-      }
-    }
-    return ans;
-  }
-  
-  int binarySearchStart(int[]nums , int target)
-  {
-    int start = 0 ;
-    int end = nums.length-1;
-    int ans = -1;
-    while(start<=end)
-    {
-      
-      int m = start + (end-start)/2;
-      
-      if(nums[m]==target)
-      {
-        ans = m ; 
-        end = m-1;
-      }
-      else if(nums[m]<target)
-      {
-        start=m+1;
-      }
-      else
-      {
-        end = m-1;
-      }
-    }
-    return ans;
-    
-  }
-  
-  
-  
+ 
 }
