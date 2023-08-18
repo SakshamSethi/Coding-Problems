c19f7d352423d3ad382b32b640e8a2c6908161b1
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-18 10:35:08 +0530

    Time: 0 ms (100.00%), Space: 49.8 MB (73.79%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,9 +1,68 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         
-       int n = 0;
+     //  int n = 0;
+        // O n approach using Xor !
+    //  for(int ele : nums) n=n^ele;
+ //     return n;
+    
       
-      for(int ele : nums) n=n^ele;
-      return n;
+      // Binary Search solution Logn approach !
+      
+      int start = 1;
+      int n=nums.length;
+      int end = n-2;
+      
+      //Boundary cases :)
+      if(n==1) return nums[0];
+      if(nums[0]!=nums[1])return nums[0];
+      if(nums[n-1]!=nums[n-2])return nums[n-1];
+      
+      int mid=0;
+      
+      while(start<=end)
+      {
+        mid = start + (end-start)/2;
+        
+        
+        if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
+        
+        else if(mid%2==1)
+        {
+          // if at odd index check if equals to previous even index --> yes : at left part 
+          //                                                        --> no : at left part
+        
+          if(nums[mid]==nums[mid-1]) // at left part
+          {
+            start = mid+1;
+          }
+          else
+          {
+            end=mid-1;
+          }
+        
+        }
+         
+        
+         else if(mid%2==0)
+        {
+          // if at even index check , if equals to next  index --> yes : at left part 
+          //                                                        --> no : at right part
+        
+          if(nums[mid]==nums[mid+1]) // at left part
+          {
+            start = mid+1;
+          }
+          else
+          {
+            end=mid-1;
+          }
+        
+        }
+        
+      }
+      
+      return start ; // Dummy :)
+      
     }
 }
