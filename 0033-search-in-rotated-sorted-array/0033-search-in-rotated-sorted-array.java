8da1cb2d14fcdacaed9f0ecd3e58832592790711
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-22 12:13:33 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (47.63%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,77 +1,114 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        
+  
+/*  int findPivot(int[]nums)
+  {
+      int start = 0;
+      int end = nums.length-1;
+      int mid = 0;        
 
-        int s=0,e=nums.length-1,mid=0;
-   
-    int pivot=0;
-       pivot = Pivot(nums);
-       
-        System.out.println(pivot);
-      if(pivot ==-1)
-      {
-          return BS(nums,s,e,target);
-          
-      }
-        else if(nums[pivot]==target)
-        {
-            return pivot;
-        }
-       else if(nums[0]<=target)
-       {
-           return BS(nums,s,pivot-1,target);
-       }
-        else
-        {
-            return BS(nums,pivot+1,e,target);
-        }
-           
-           
-       
-       
-    }
-    static int BS(int[] nums , int s,int e,int target)
+    while(start<=end)
     {
-        int mid=0;
-        while(s<=e)
-        {
       
-            mid = s+(e-s)/2;
-        System.out.println(mid);
-            if(nums[mid]==target)
-                return mid;
-            else if(nums[mid]>target)
-                e=mid-1;
-            else
-                s=mid+1;
-        }
-        
-        return -1;
-    }
-static int Pivot(int[] nums)
-{
-   int s=0,e=nums.length-1,mid=0;
-     while(s<=e)
-        {
-            mid = (s + (e-s)/2);
-           
-         if(mid<e && nums[mid]>nums[mid+1])
-         {return mid;
-         }
-           if(mid>s && nums[mid-1]>nums[mid] )
-           {      return mid-1;
-           }
-         if(nums[mid]<=nums[s])
-         {
-             e=mid-1;
-         }
-         else
-         {
-             s=mid+1;
-        }
-     }
+      mid = start +(end-start)/2;
+      
+      if(mid<end && nums[mid]>nums[mid+1] ) // to check the next element 
+      return mid;
+      
+      else if(mid>start && nums[mid]<nums[mid-1])
+        return mid-1;
+      
+      else if(nums[mid]<nums[start])
+        end = mid-1;
+      
+      else if(nums[mid]>nums[start])
+        start=mid+1;
+      
+      }
+    return -1; // Dummy return ;
     
-    return -1;    
-}
+  }
+  */
+  
+  
+  public int search(int[] nums, int target) {
 
+      
+    /* int pivot = findPivot(nums);
+    
+    // Apply Binary Search in Both the halves :
+    
+    int res = binarySearch(nums,0,pivot,target);
+    
+    return res!=-1?res:binarySearch(nums,pivot+1,nums.length-1,target);
+    
+    */
+    
+    
+    // without the pivot approach :
+    
+    
+    // we need to thing about how can we reduce out search space 
+    
+    
+    // there will be 2 sorted halves as the array is rotated --> we need to check that cuurently at which half are we and then we will check the range that if the target lies with in that part or not and we will eliminate the search space accordingly 
+    
+    
+    int start =0;
+    int end = nums.length-1;
+    int mid =0;
+    
+    while(start<=end)
+    {
+      mid = start + (end-start)/2;
+      
+      if(nums[mid]==target) return mid ;
+      
+      // if we are at the left sorted part 
+      
+     else if(nums[start]<=nums[mid])
+      {
+        // check the range 
+        
+        if(nums[start]<= target && nums[mid]>target) // target lies with in this range 
+          end = mid-1 ;
+        
+        else
+          start=mid+1;
+        
+        
+      }
+      // we are at right sorted part 
+      else
+      {
+        if(nums[end]>=target && target>nums[mid])
+          start=mid+1;
+        else 
+          end=mid-1;
+        
+      }
+      
+    }
+    
+    return -1;
+    
+    }
+  
+  /*int binarySearch(int[]nums,int start , int end , int target)
+  {
+    int mid =0;
+    
+    while(start<=end)
+    {
+      mid = start +(end-start)/2;
+      
+      if(nums[mid]==target) return mid;
+      
+      else if(nums[mid]<target) start=mid+1;
+      
+      else end=mid-1;
+    }
+    
+    return -1;
+  }
+  */
 }
