54dfeb6d00c78ea44de2bec8dc4997ac01d1f1ec
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-22 13:24:40 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (63.51%) - LeetHub

--- 0754-reach-a-number/0754-reach-a-number.java
+++ 0754-reach-a-number/0754-reach-a-number.java
@@ -1,23 +1,47 @@
 class Solution {
     public int reachNumber(int target) {
-
-        target = Math.abs(target);
-int sum=0 , steps=0;
-   
-    while(sum<target)
-    {
-        steps++;
-        sum+=steps;
-    }
-        while((sum-target)%2==1)
-        {
-            steps++;
-            sum+=steps;
+        
+		//convert negative target to positive
+        if(target<0){
+            target*=-1;
         }
-    return steps;
-    }
-    
-    
-    
-
+        
+        int start = 1;
+        int end = target; 
+        int numMoves = 0;
+        long finalPosition = 0;
+        
+        while(start<=end){
+            int mid = start+(end-start)/2;
+			
+			// here we consider n = mid then n*(n+1)/2 becomes mid*(mid+1)/2
+            long sum = (long)mid*(mid+1)/2;
+            
+			
+            if(sum>=target){
+			//if difference is even direct binary search
+                finalPosition = sum;
+                numMoves = mid;
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+              }
+          }
+            
+        int difference = (int)(finalPosition - target);
+                //if difference is odd
+                if(difference%2!=0){
+                    
+					//if n+1 is odd
+                    if((numMoves+1)%2!=0){
+                        numMoves+=1;
+                    }
+                    else{
+					//if n+1 is even, then n+2 is odd
+                        numMoves+=2;
+                    }  
+                }
+     return numMoves;           
+    }        
 }
