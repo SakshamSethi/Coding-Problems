5e843695c69be3d4f2cd593a3ea5f351f73d19ff
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-23 17:25:04 +0530

    Time: 4 ms (87.61%), Space: 57 MB (25.78%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -3,11 +3,55 @@ class Solution {
      
       // easy brute force 
       
-      Arrays.sort(nums);
+/*      Arrays.sort(nums);
       
       for(int i=0 ; i<nums.length-1;i++)
         if(nums[i]==nums[i+1]) return nums[i];
       
+      return -1; */
+      
+      // without modifying and constant space : we can use  double for loop 
+      // brute force without modifying!
+  
+      /* TLE 
+      int n = nums.length;
+      for(int i=0 ; i<n ; i++)
+      {
+        for(int j=0; j<n ; j++)
+        {
+          if(i==j)continue;
+          if(nums[i]==nums[j]) return nums[i];
+        }
+      }
+      
       return -1;
+      */
+      
+      // can also do map stuff to store frequency but we dont have to use extra space !
+      
+      // using slow and fast pointer approach 
+      
+      int n = nums.length;
+      int slow = 0 ;
+      int fast = 0 ;
+        
+      do
+      {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        
+      }while(slow!=fast);
+      
+      
+      int slow2=0;
+      
+      while(slow2!=slow)
+      {
+        slow = nums[slow];
+        slow2=nums[slow2];
+      }
+      
+      return slow;
+      
     }
 }
