cc5c16ca51bc172ede231cf4740d8549a731975b
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-23 18:06:50 +0530

    Time: 9 ms (97.62%), Space: 45.5 MB (16.30%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,24 +1,80 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int D) {
-        int left = 0, right = 0;
-        for (int w: weights) {
-            left = Math.max(left, w);
-            right += w;
+    public int shipWithinDays(int[] weights, int days) {
+     
+      
+      // from the given question we can conclude that 
+      
+      // the minimum capacity of the ship should be atleast that much  that it can load the maxmimum wieght package , so the minimum capacity that the ship must have should be the maximum weight element 
+      
+      // And in the worst case the ship should have the capacity of sum of all the elements as if the no of days we have is only 1 then we would have to ship all the packages at one go 
+      
+  // so we can use the binary search to check if the capacity which we will dervive using mid formulae can this be the capacity which would be able to ship all the packages on time 
+      
+      // and we will make changes to the min and max (low and high) accordingly !
+      
+      
+      
+      int minCapacity = 0;
+      int maxCapacity = 0;
+      
+      for(int weight : weights)
+      {
+        minCapacity = Math.max(minCapacity , weight);
+        maxCapacity += weight;
+      }
+      
+      // now we can apply binary search approach (lowerBound and UpperBound) to find out the minimum capacity the ship should have 
+      
+      
+      int ans = 0;
+      
+      while(minCapacity<=maxCapacity)
+      {
+        int curCapacity = minCapacity + (maxCapacity-minCapacity)/2;
+        
+        // check if the current capacity is capable of transferring all the packages with in the D days given 
+        
+        
+        if(canShip(weights,days,curCapacity))
+        {
+          ans = curCapacity ;
+          maxCapacity = curCapacity-1;
         }
-        while (left < right) {
-            int mid = (left + right) / 2, need = 1, cur = 0;
-            for (int w: weights) {
-                if (cur + w > mid) {
-                    need += 1;
-                    cur = 0;
-                }
-                cur += w;
-            }
-            if (need > D) left = mid + 1;
-            else right = mid;
+        else
+        {
+          minCapacity=curCapacity+1;
         }
-        return right;
+        
+        
+      }
+      
+      return ans;
+      
     }
-    
-   
+  
+  boolean canShip(int[]weights , int days,int capacity)
+  {
+    // i have to check that weather we can ship withint he limited days
+  
+  
+    int daysRequired = 1; 
+    int sum = 0;    
+    for(int i = 0 ; i<weights.length ; i++)
+    {
+      sum+=weights[i];
+      
+      if(sum>capacity)
+      {
+        daysRequired++;
+        sum = weights[i];
+        
+      }
+ 
+    }
+     
+      return daysRequired<=days;
+  }
+  
+  
+  
 }
