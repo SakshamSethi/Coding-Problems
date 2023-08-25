177d98e5080df7067332c3d2fcbb66671cd0ca93
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-25 18:52:19 +0530

    Time: 0 ms (100.00%), Space: 81.5 MB (90.99%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -1,26 +1,40 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
-        int startCol = 0, endCol = mat[0].length-1;
+      
+      // we will try to eliminate the columns 
+      
+      int startCol = 0;
+      int endCol = mat[0].length-1;
+      
+      int maxRow=0;
+      
+      while(startCol<=endCol)
+      {
+        int midCol = startCol + (endCol-startCol)/2;
         
-        while(startCol <= endCol){
-            int maxRow = 0, midCol = startCol + (endCol-startCol)/2;
-            
-            for(int row=0; row<mat.length; row++){
-                 maxRow = mat[row][midCol] >= mat[maxRow][midCol]? row : maxRow;  
-            }
-            
-            boolean leftIsBig    =   midCol-1 >= startCol  &&  mat[maxRow][midCol-1] > mat[maxRow][midCol];
-            boolean rightIsBig   =   midCol+1 <= endCol    &&  mat[maxRow][midCol+1] > mat[maxRow][midCol];
-            
-            if(!leftIsBig && !rightIsBig)   // we have found the peak element
-                return new int[]{maxRow, midCol};
-            
-            else if(rightIsBig)  // if rightIsBig, then there is an element in 'right' that is bigger than all the elements in the 'midCol', 
-                startCol = midCol+1; //so 'midCol' cannot have a 'peakPlane'
-            
-            else // leftIsBig
-                endCol = midCol-1;
+        // find out the max element in the midCol  and store its row value in maxRow
+        
+        // use a for loop for that row
+        
+        for(int i=0;i<mat.length;i++)
+        {
+          if(mat[i][midCol]>=mat[maxRow][midCol])
+          {
+            maxRow = i;
+          }
         }
-        return new int[]{-1,-1};
+      
+        boolean isLeftBig = (midCol>startCol && mat[maxRow][midCol]<= mat[maxRow][midCol-1])?true:false;
+        boolean isRightBig = (midCol<endCol && mat[maxRow][midCol]<= mat[maxRow][midCol+1])?true:false;
+        
+      if(!isLeftBig && !isRightBig) return new int[]{maxRow,midCol};
+      
+      else if(isLeftBig) endCol = midCol-1;
+        
+      else if(isRightBig)startCol = midCol+1;
+        
+      }
+      
+      return new int[]{-1,-1};
     }
 }
