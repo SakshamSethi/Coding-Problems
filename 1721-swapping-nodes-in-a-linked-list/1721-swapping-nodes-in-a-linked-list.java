f9370444463aaec82cdf9d25922048bb29103c35
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-29 13:35:11 +0530

    Time: 2 ms (78.30%), Space: 57 MB (75.81%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,31 +10,54 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        
-        if(head==null||head.next==null)return head;
-        
-        ListNode temp=head;
-        for(int i=1;i<k;i++)
-        {
-            temp=temp.next;
-        }
-        ListNode Last = head;
-        int length=1;
-        while(Last.next!=null)
-        {
-            Last=Last.next;
-            length++;
-        }
-        int skip = length-k;
-        
-        ListNode temp2 =head;
-        for(int i=1;i<=skip;i++)
-        {
-            temp2=temp2.next;
-        }
-        int swap = temp.val;
-        temp.val=temp2.val;
-        temp2.val = swap;
-        return head;
+      
+      if(head==null || head.next==null)return head;
+       
+      int size = linkedListLength(head);
+      
+      ListNode fromFront = head;
+      
+      ListNode temp = head;
+      
+      for(int i=1 ; i<k;i++)
+      {
+        temp=temp.next;
+      }
+      
+      fromFront = temp;
+      temp = head;
+      ListNode fromBack = head;
+      
+      int backIndex = size-k+1;
+      
+      for(int i=1 ; i<backIndex;i++)
+      {
+        temp = temp.next;
+      }
+      
+      fromBack = temp;
+      
+      int tempVal = fromFront.val;
+      fromFront.val = fromBack.val;
+      fromBack.val = tempVal;
+      
+      return head;
+      
+      
+      
     }
+  
+  int linkedListLength(ListNode head)
+  {
+    int size = 1;
+    ListNode temp = head;
+    
+    while(temp.next!=null)
+    {
+      temp=temp.next;
+      size++;
+      
+    }
+    return size;
+  }
 }
