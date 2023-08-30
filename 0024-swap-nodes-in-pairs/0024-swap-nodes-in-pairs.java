b4046977a22624f5010444c46eddf0181f5bb331
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-30 10:12:48 +0530

    Time: 0 ms (100.00%), Space: 40 MB (65.15%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -10,20 +10,51 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        if(head ==null || head.next==null)return head;
-        ListNode curr = head;
-        ListNode next = head.next;
      
-        do{
-            int temp = curr.val ;
-            curr.val = next.val;
-            next.val = temp;
-            
-            curr = next.next;
-            if(curr==null || curr.next==null) break;
-            next=curr.next;
-            
-        }   while(next!=null);
-        return head;
+      if(head==null || head.next==null) return head;
+      
+      ListNode dummyFirst = new ListNode();
+      ListNode dummySecond = new ListNode();
+      ListNode first = dummyFirst;
+      ListNode second = dummySecond;
+      
+      ListNode temp = head;
+      while(temp!=null)
+      {
+        ListNode node1 = new ListNode(temp.val);
+        first.next = node1;
+        first=first.next;
+        
+        temp=temp.next;
+        if(temp==null)break;
+        
+        ListNode node2 = new ListNode(temp.val);
+        second.next = node2;
+        second=second.next;
+        temp=temp.next;
+        
+      }
+      
+      first = dummyFirst.next;
+      second = dummySecond.next;
+      
+  
+      while(second!=null && first!=null)
+      {
+        
+        ListNode nextRef = second.next;
+        second.next=first;
+        second=nextRef;
+        
+        if(second==null || first.next==null) break;
+         
+        
+        nextRef = first.next;
+        first.next=second;
+        first=nextRef;
+       
+      }
+      
+      return dummySecond.next;
     }
 }
