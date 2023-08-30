59a7dd0d74a4b83b83dd1b30f87298cef6c8dd8b
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-30 16:47:22 +0530

    Time: 3 ms (93.12%), Space: 42.9 MB (85.46%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -9,34 +9,50 @@
  * }
  */
 class Solution {
-    public ListNode insertionSortList(ListNode head) {
-      
-      if(head==null || head.next==null)return head;
-      
-      ListNode dummy = new ListNode();
-      
-      ListNode curr = head, next=null;
-      
-      while(curr!=null)
-      {
-         next = curr.next;
-        
-        ListNode pre = dummy;
-         
-        while(pre.next!=null && pre.next.val<curr.val)
-        {
-          pre=pre.next;
-        }
-
-        curr.next=pre.next;
-        
-        pre.next=curr;
-        
-        curr = next;
-        
-      }
-      
-      return dummy.next;
-      
+   public ListNode insertionSortList(ListNode head) {
+    if (head == null || head.next == null)
+    {
+        return head;
     }
+
+    ListNode sortedHead = head, sortedTail = head;
+    head = head.next;
+    sortedHead.next = null;
+    
+    while (head != null)
+    {
+        ListNode temp = head;
+        head = head.next;
+        temp.next = null;
+        
+        // new val is less than the head, just insert in the front
+        if (temp.val <= sortedHead.val)
+        {
+            temp.next = sortedHead;
+    
+            sortedHead = temp;
+        }
+        // new val is greater than the tail, just insert at the back
+        else if (temp.val >= sortedTail.val)
+        {
+            sortedTail.next = temp;
+            sortedTail = sortedTail.next;
+        }
+        // new val is somewhere in the middle, we will have to find its proper
+        // location.
+        else
+        {
+            ListNode current = sortedHead;
+            while (current.next != null && current.next.val < temp.val)
+            {
+                current = current.next;
+            }
+            
+            temp.next = current.next;
+            current.next = temp;
+        }
+    }
+    
+    return sortedHead;
+}
 }
