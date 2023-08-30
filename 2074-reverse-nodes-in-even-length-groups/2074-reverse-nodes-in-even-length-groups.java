536b4fdac5393b46155b66ef4df73814d32e086b
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-08-30 20:28:51 +0530

    Time: 5 ms (96.35%), Space: 68.4 MB (53.80%) - LeetHub

--- 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.java
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.java
@@ -10,39 +10,42 @@
  */
 class Solution {
     public ListNode reverseEvenLengthGroups(ListNode head) {
-   
-        
-    ListNode temp = head;
-    int num=1;
-    Stack st = new Stack();
-    while(temp!=null)
-    {
-        int count=0;
-        ListNode start = temp;
-        
-        
-        while(count!=num && temp!=null)
-        {
-            st.push(temp.val);
-            temp=temp.next;
-            count++;
-                
-        }
-        
-        if(count%2==0)
-        {
-            while(temp!=start)
-            {
-              start.val=(int) st.pop();
-             start=start.next;  
+
+        //  if LL has 1 or 2 nodes only
+        if (head.next == null || head.next.next == null) return head;
+        ListNode node = head;
+        int group = 1;
+
+        while (node != null && node.next != null) {
+            group++;
+			
+            //first check number of nodes 
+            int countNodes = 0;
+            ListNode temp = node.next;
+            while (temp != null && countNodes < group) {
+                temp = temp.next;
+                countNodes++;
             }
-            
+			
+			//now we have count now we will check if it is even then do reverse else skip
+            if (countNodes % 2 == 0) {
+                ListNode curr = node.next, prev = null, next = null;
+                for (int i = 0; i < countNodes; i++) {
+                    next = curr.next;
+                    curr.next = prev;
+                    prev = curr;
+                    curr = next;
+                }
+
+                ListNode tail = node.next;
+                tail.next = curr;
+                node.next = prev;
+                node = tail;
+            } else {
+                for (int i = 0; i < countNodes; i++)
+                    node = node.next;
+            }
         }
-        
-        num++;
-    }
-    
-    
-    return head;
+        return head;
     }
 }
