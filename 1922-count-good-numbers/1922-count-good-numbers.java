00ee9a88b27e94729b729a9b0e8ce0d1a48b54ff
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-09-05 21:57:16 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (90.41%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.java
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -1,19 +1,31 @@
 class Solution {
-    public long mod = 1000000007; 
+  public long mod = 1000000007;
     public int countGoodNumbers(long n) {
-        
+  
     
-        long odd = n/2; 
-        long even = (n+1)/2; 
-        return (int)(pow(5, even)*pow(4, odd)%mod);
+      // observation : its kind of a permutation solution :
+      // if we observe carefully then all we need to find is that how many places in that number is going to be even and odd postions;
+      
+// at even index we are having only even numbers from 0-9 we have 5 even digits (0,2,4,6,8)
+// at each  even  indexe we can have 5 ways to build a number -> 5^(no of even indexes)
+// similarly at odd indexes we have only 4 prime no.s to adjust (1,3,5,7)
+      
+      
+    long even = (n+1)/2 ; // 0 based indexing
+      long odd = n/2;
+     
+      long evenPart = myPow(5,even)%mod;
+      long oddPart = myPow(4,odd)%mod;
+      
+      long ans = (evenPart*oddPart)%mod;
+      return (int)ans;
+      
     }
-    
- 
-    public long pow(long x, long n) {
+ public long myPow(long x, long n) {
         
         if(n==0) return 1;
         
-        long temp = pow(x,n/2);
+        long temp = myPow(x,n/2);
         
         if(n%2==0){
             return (temp*temp)%mod;
