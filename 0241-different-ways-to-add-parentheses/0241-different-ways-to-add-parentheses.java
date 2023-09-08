091cf67a848f77e7db5b443319b230bd966fff87
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-09-08 20:02:50 +0530

    Time: 1 ms (100.00%), Space: 41.6 MB (65.18%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,44 +1,35 @@
 class Solution {
     public List<Integer> diffWaysToCompute(String e) {
-     
-        List<Integer> ans = new ArrayList<>();
         
-        for(int i=0;i<e.length();i++)
+      List<Integer> ans = new ArrayList<>();
+      
+      for(int i=0 ; i<e.length();i++)
+      {
+        char ch = e.charAt(i);
+        if(ch=='+'||ch=='-'||ch=='*')
         {
-            if(e.charAt(i)=='+'|| e.charAt(i)=='*'||e.charAt(i)=='-')
+          List<Integer> left = diffWaysToCompute(e.substring(0,i));
+          List<Integer> right = diffWaysToCompute(e.substring(i+1));
+          
+          for(int a : left)
+          {
+            for(int b: right)
             {
-            List<Integer> left = diffWaysToCompute(e.substring(0,i));
-            List<Integer> right = diffWaysToCompute(e.substring(i+1));
-            int c=0;
-                for(Integer a : left)
-                {
-                    for(Integer b : right)
-                    {
-                        switch(e.charAt(i))
-                        {
-                            case '+': c=a+b;
-                                break;
-                                case '-': c=a-b;
-                                break;
-                                case '*': c=a*b;
-                                break;
-                                
-                                
-                        }
-                                ans.add(c);
-                    }
-                }
-        
-                
+              switch(ch)
+              {
+                  case '+' : ans.add(a+b);
+                  break;
+                  case '-' : ans.add(a-b);
+                  break; 
+                 case '*' : ans.add(a*b);
+                  break;
+              }
             }
-            
+          }
           
         }
-          if(ans.size()==0)
-            {
-                ans.add(Integer.parseInt(e));
-            }
-            return ans;
-        
+      }
+      if(ans.size()==0) ans.add(Integer.parseInt(e));
+      return ans;
     }
 }
