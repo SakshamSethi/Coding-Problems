0633964725706c0a8ba367d4c607aa242b7c72fb
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-09-21 22:00:49 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (32.96%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1360-number-of-days-between-two-dates/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1360-number-of-days-between-two-dates.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1360-number-of-days-between-two-dates/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1360-number-of-days-between-two-dates.java
@@ -1,22 +1,26 @@
+ 
 class Solution {
-int[] month = {31,28,31,30,31,30,31,31,30,31,30,31};
-    private int[] months = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        
     public int daysBetweenDates(String date1, String date2) {
-        return Math.abs(from1971(date1) - from1971(date2));
+        return Math.abs(numOfDays(date1) - numOfDays(date2));
     }
-    
-    private int from1971(String date) {
-        String[] parts = date.split("-");
-        int year = Integer.parseInt(parts[0]), month = Integer.parseInt(parts[1]), day = Integer.parseInt(parts[2]);
-        int count = day;
-        for (int i = 1971; i < year; i++) count += (isLeapYear(i)) ? 366 : 365;
-        for (int i = 0; i < month - 1; i++) count += months[i];
-        if (month > 2 && isLeapYear(year)) count += 1;
-        return count;
-    }
-    
-    private boolean isLeapYear(Integer year) {
-        return year % 400 == 0 || (year % 100 != 0 && year % 4 == 0);
+
+    public int numOfDays(String date) {
+        int[] monthDays = {
+            31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+            };
+
+        int year = Integer.parseInt(date.substring(0,4));
+        int month = Integer.parseInt(date.substring(5,7));
+        int day = Integer.parseInt(date.substring(8,10));
+
+        for(int i = 0 ; i < month-1; i++){
+            day += monthDays[i];
+        }
+
+        day += (year-1)*365 + (year-1)/4;
+        if(year % 4 == 0 && month > 2 && year != 2100) day++;
+
+        return day;
     }
 }
+ 
