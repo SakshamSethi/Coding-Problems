21533938e2e0226ba9da73cb59a89ddc81cdd97a
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-10-18 21:00:59 +0530

    Time: 5 ms (43.88%), Space: 41.7 MB (6.99%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,62 +1,63 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        if(digits==null || digits.length()==0) return new ArrayList<String>();
-        return pad("",digits);
+        
+      List<String>ans = new ArrayList<>();
+      if(digits.length()==0) return  ans;
+      
+      combination("",digits,ans);
+      
+      return ans;
+      
+      
     }
-    List<String> pad(String p , String up)
+  
+  void combination(String p , String up , List<String>ans)
+  {
+    if(up.length()==0)
     {
-        if(up.isEmpty())
-        {
-            List<String> str = new ArrayList<String>();
-     if(p.isEmpty())
-     {
-         return str;
-     }
-            str.add(p);
-            return str;
-        }
-        
-        List<String> list = new ArrayList<>();
-        
-        
-        int digit = up.charAt(0) -'0';
-    
-        if(digit==7)
-        { 
-            
-            
-            for(int i=(digit-3)*3 + 3;i<(digit-2)*3+4;i++)
-            {
-                char ch = (char)('a'+i);
-                list.addAll(pad(p+ch,up.substring(1)));
-            }
-        }
-        else if(digit==8)
-        {
-           for(int i=(digit-3)*3 +4;i<(digit-3)*3+7;i++)
-            {
-                char ch = (char)('a'+i);
-                list.addAll(pad(p+ch,up.substring(1)));
-            } 
-        }
-         else  if(digit==9)
-        { 
-            
-            
-            for(int i=22;i<26;i++)
-            {
-                char ch = (char)('a'+i);
-                list.addAll(pad(p+ch,up.substring(1)));
-            }
-        }
-        else
-        {
-            for(int i=(digit-2)*3;i<(digit-1)*3;i++)
-            {
-                char ch = (char)('a'+i);
-                list.addAll(pad(p+ch,up.substring(1)));
-            }
-        }
-        return list;
+      ans.add(p);
+      return ;
     }
+  
+  
+  int digit = up.charAt(0)-'0';
+  
+  if(digit >=2 && digit<=6)
+  {
+    
+    for(int i=(digit-2)*3 ; i<(digit-1)*3 ; i++)
+    { 
+      char ch = (char)('a'+i);
+      combination(p+ch,up.substring(1),ans);
+      
+    }
+    
+  }
+  
+  if(digit==7)
+  {
+     for(int i=(digit-2)*3 ; i<=(digit-1)*3 ; i++)
+    {
+    char ch = (char)('a'+i);
+      combination(p+ch,up.substring(1),ans);
+    }
+  }
+  if(digit==9)
+  {
+     for(int i=((digit-2)*3)+1 ; i<=((digit-1)*3)+1 ; i++)
+    {
+     char ch = (char)('a'+i);
+      combination(p+ch,up.substring(1),ans);
+    }
+  }
+  if(digit==8)
+  {
+     for(int i=((digit-2)*3)+1 ; i<=(digit-1)*3 ; i++)
+    {
+      char ch = (char)('a'+i);
+      combination(p+ch,up.substring(1),ans);
+    }
+  }
+  } 
+  
 }
