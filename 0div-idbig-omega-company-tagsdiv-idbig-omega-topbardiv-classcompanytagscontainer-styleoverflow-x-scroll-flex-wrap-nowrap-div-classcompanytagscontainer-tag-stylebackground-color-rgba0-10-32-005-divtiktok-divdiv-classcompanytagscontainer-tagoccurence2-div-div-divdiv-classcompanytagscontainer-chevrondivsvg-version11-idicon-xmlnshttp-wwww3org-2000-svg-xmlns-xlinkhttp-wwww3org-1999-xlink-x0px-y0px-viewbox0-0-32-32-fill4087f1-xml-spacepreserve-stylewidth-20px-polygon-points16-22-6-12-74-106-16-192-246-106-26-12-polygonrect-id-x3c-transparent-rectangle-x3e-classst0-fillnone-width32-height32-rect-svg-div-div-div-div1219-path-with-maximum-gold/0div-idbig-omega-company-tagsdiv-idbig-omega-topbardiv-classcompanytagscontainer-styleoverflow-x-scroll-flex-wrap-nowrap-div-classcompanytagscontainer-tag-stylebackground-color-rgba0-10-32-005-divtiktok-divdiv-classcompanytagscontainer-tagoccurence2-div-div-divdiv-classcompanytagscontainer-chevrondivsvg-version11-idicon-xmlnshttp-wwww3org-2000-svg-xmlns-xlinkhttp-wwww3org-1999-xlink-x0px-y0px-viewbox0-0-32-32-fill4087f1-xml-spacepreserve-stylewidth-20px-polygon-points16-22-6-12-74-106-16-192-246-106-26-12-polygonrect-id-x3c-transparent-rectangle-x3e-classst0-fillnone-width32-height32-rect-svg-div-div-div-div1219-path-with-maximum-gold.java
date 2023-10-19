4846bb1127ebaead48d89cb324cb8d0a4246c5fe
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-10-19 17:41:00 +0530

    Time: 57 ms (68.36%), Space: 40.2 MB (42.09%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1219-path-with-maximum-gold/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1219-path-with-maximum-gold.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int getMaximumGold(int[][] grid) {
+        
+        int maxGold = -1;
+        for(int i=0; i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                maxGold = Math.max(maxGold , compute(grid,i,j));
+                
+            }
+        }
+        return maxGold;
+    }
+    int compute(int[][]grid , int i , int j )
+    {
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0 || grid[i][j]==-1)
+            return 0;
+        
+        int val = grid[i][j];
+        grid[i][j]=-1;
+        int left = val + compute(grid,i,j-1);
+        
+        int right = val + compute(grid,i,j+1);
+        int up = val + compute(grid,i-1,j);
+        int down = val + compute(grid,i+1,j);
+            
+        grid[i][j]=val;
+        
+        return Math.max(left,Math.max(right,Math.max(up,down)));
+    }
+}
