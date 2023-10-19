15a3c02dec794162bf1fa71d3321c0157eb5f7ef
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-10-19 17:46:05 +0530

    Time: 56 ms (75.67%), Space: 40.6 MB (22.69%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,36 +1,32 @@
 class Solution {
     public int getMaximumGold(int[][] grid) {
         
-        int MaxG=0;
-        for(int i=0;i<grid.length;i++)
+        int maxGold = -1;
+        for(int i=0; i<grid.length;i++)
         {
             for(int j=0;j<grid[0].length;j++)
             {
-                MaxG = Math.max(MaxG,calc(grid,i,j));
+                maxGold = Math.max(maxGold , compute(grid,i,j));
+                
             }
         }
-        return MaxG;
+        return maxGold;
     }
- static int calc(int[][] grid,int r,int c )
- {
-     
-     
-     if(r<0||r>=grid.length||c<0||c>=grid[0].length||grid[r][c]==0)
-     {
-         return 0;
-     }
-     int temp = grid[r][c];
-
-     grid[r][c]=0;
-     
-     int mA = calc(grid,r+1,c);
-     int mB = calc(grid,r,c+1);
-     int mC = calc(grid,r,c-1);
-     int mD = calc(grid,r-1,c);
-    
-     grid[r][c]=temp;
- return temp+Math.max(mA,Math.max(mB,Math.max(mC,mD)));
-     
-     
- }
+    int compute(int[][]grid , int i , int j )
+    {
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0 || grid[i][j]==-1)
+            return 0;
+        
+        int val = grid[i][j];
+        grid[i][j]=-1;
+        int left = val + compute(grid,i,j-1);
+        
+        int right = val + compute(grid,i,j+1);
+        int up = val + compute(grid,i-1,j);
+        int down = val + compute(grid,i+1,j);
+            
+        grid[i][j]=val;
+        
+        return Math.max(left,Math.max(right,Math.max(up,down)));
+    }
 }
