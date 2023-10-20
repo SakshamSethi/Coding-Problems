b79e4aa0347a315186d2861f4fbdb08ec946c957
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-10-20 20:10:08 +0530

    Time: 3 ms (61.53%), Space: 43.1 MB (99.42%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,82 +1,82 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        
         char[][] board = new char[n][n];
-            for(int i=0;i<n;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    board[i][j]='.';
-                }
-            }
-        List<List<String>> ans = new ArrayList<>();
-        queens(board,0,ans);
-        return ans;
+        
+        List<List<String>>ans = new ArrayList<>();
+        
+        compute(board ,0, n , ans);
+        
+        return ans ;
+        
+            
     }
-    static void queens(char[][] board , int row , List<List<String>>res)
-        {
-            if(row==board.length)
-            {
-                construct(board,res);
-                return ;
-            }
-        for(int col =0;col<board.length;col++)
-        {
-            
-            if(safe(board,row,col))
-            {
-            board[row][col]='Q';
-            queens(board,row+1,res);
-                board[row][col]='.';
-        }
-            
-        }
-        return;
-        }
-    static boolean safe(char[][] board,int r,int c)
+    
+    void compute(char[][]board , int i   , int n , List<List<String>>ans)
     {
-        int i=0;
-        // For Upper Rows
-        for(i=0;i<r;i++)
+        if(i==n)
         {
-            if(board[i][c]=='Q')
-                return false;
+            construct(board, ans);
         }
-        // For left Diagonal 
-        int maxL = Math.min(r,c);
-          for(i=0;i<=maxL;i++)
+        
+         
+        for(int j=0 ; j<n ; j++)
         {
-            if(board[r-i][c-i]=='Q')
-                return false;
+            if(isSafe(board,i,j,n))
+            {
+                board[i][j]='Q';
+                compute(board,i+1,n,ans);
+                board[i][j]='.';
+            }
         }
-        int maxR = Math.min(r,(board.length-c-1));
-          for(i=0;i<=maxR;i++)
+        
+    }
+    boolean isSafe(char[][]board , int i , int j,int n)
+    {
+        // check same col 
+        int row = i-1;
+        int col = j;
+        while(row>=0)
         {
-            if(board[r-i][c+i]=='Q')
-                return false;
+            if(board[row][j]=='Q') return false;
+            row--;
         }
+        row= i-1;
+        col=j-1;
+        while(row>=0 && col>=0)
+        {
+            if(board[row][col]=='Q')return false;
+            row--;
+            col--;
+        }
+         row = i-1 ;
+        col = j+1;
+        while(row>=0 && col<n)
+        {
+            if(board[row][col]=='Q') return false;
+            row--;
+            col++;
+        }
+        
         return true;
     }
-    static void construct(char[][] board,List<List<String>> res)
-    {
-        List<String> str = new ArrayList<>();
-        
-for(int i=0;i<board.length;i++)
-{
-    StringBuilder s = new StringBuilder();
-  
     
-    for(int j=0;j<board.length;j++)
+    void construct(char[][]board , List<List<String>>ans)
     {
-        if(board[i][j]=='Q')
-        { s=s.append("Q");
-        }else
-            s=s.append(".");
-    }
-
-        str.add(new String(s));
-}
+        List<String> temp = new ArrayList<>();
         
-        res.add(new ArrayList<String>(str));
+       
+        
+        for(char[] row : board)
+        {
+          StringBuilder str = new StringBuilder();
+            
+            for(char ch : row)
+            {
+                if(ch=='Q') str.append('Q');
+                else str.append('.');
+            }
+            temp.add(str.toString());
+        }
+        ans.add(new ArrayList(temp));
     }
 }
