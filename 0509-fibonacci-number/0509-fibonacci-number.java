bc97120c5677763666f080deaacc45b500c2b454
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-10-22 19:32:27 +0530

    Time: 25 ms (7.22%), Space: 43.1 MB (6.66%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,81 +1,15 @@
-    
-    class Solution {
-   
-    /*public int fib(int n) {
+class Solution {
+
+    public int fib(int n) {
         
-        return f(n);
+    int[]dp = new int[n+1];
+        return solve(n,dp);
+        
     }
-    static int f(int n)
+    int solve(int n , int[]dp)
     {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        
-        return f(n-1)+f(n-2);
-    }*/
-    
-    
-    // DP solution : MEMOIZATION : Top-Down Approach
-    
-  /*int[] dp = new int[31];
-    
-    public int fib(int n)
-    {
-        if(n<=1)
-            return n;
-        
-        else if(dp[n]!=0)
-            return dp[n];
-        
-        else 
-            return dp[n]= fib(n-1)+fib(n-2);
+                if(n<=1) return n;
+        if(dp[n]!=0)return dp[n];
+        return dp[n]= fib(n-1)+fib(n-2);
     }
-    */
-    
-        // Using Tabulation
-        
- /*       public int fib(int n)
-        {
-            if(n==0)return 0;
-            if(n==1)return 1;
-            
-            int[] dp = new int[n+1];
-            
-            
-            Arrays.fill(dp,-1);
-            dp[0]=0;
-            dp[1]=1;
-            
-            for(int i=2; i<=n; i++)
-            {
-                dp[i]= dp[i-1]+dp[i-2];
-            }
-           
-            return dp[n];
-        }
-      */  
-        // const space 
-        
-   
-            public int fib(int n)
-        {
-            if(n==0)return 0;
-            if(n==1)return 1;
-            
-           int prev =1 ;
-     int prev2 =0;
-     int cur =0;
-           
-            for(int i=2; i<=n; i++)
-            {
-                cur = prev + prev2;
-                prev2 = prev;
-                prev=cur;
-            }
-           
-            return prev;
-        }
 }
-
-
-
-
