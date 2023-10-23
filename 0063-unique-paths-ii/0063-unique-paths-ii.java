3a32544716298514227602e94aa3e545c853fece
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2023-10-23 20:27:45 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (74.02%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,54 +1,22 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
-      
-      int m = grid.length -1 ;
-      int n = grid[0].length-1;
-      
-      if(grid[m][n]==1) return 0;
-      
-      int[][] dp = new int[m+1][n+1];
-     // for(int[]arr : dp)
-    //  {
-    //    Arrays.fill(arr,-1);
-    //  }
-      
-      //return count(grid,0,0,m,n,dp);
-     
-      // Tabulation :
-      
-   dp[m][n]=1;
-      
-      for(int a =m ;a>=0;a--)
-      {
-        for(int b =n ; b>=0;b--)
-        {
-          
-          if(a==m && b==n) continue;
         
-            if(grid[a][b]==1) { dp[a][b]=0; }
-          else
-          {
-            int down =0 , right=0;
-            if(a<m) down = dp[a+1][b];
-            if(b<n) right = dp[a][b+1];
-          dp[a][b]= down+right;
-          }
-          
-        }
-      }
-      return dp[0][0];
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][]dp = new int[m][n];
+        for(int[]arr:dp) Arrays.fill(arr,-1);
+        return solve(grid , 0,0,m-1,n-1 , dp);
+        
+        
     }
-  /*int count(int[][] grid , int a , int b , int m , int n,int[][]dp)
-  {
-    if(a==m && b==n)
-      return 1;
-    
-    if(a>m||b>n||grid[a][b]==1) return 0;
-    
-    if(dp[a][b]!=-1)return dp[a][b];
-    int down = count(grid,a+1,b,m,n,dp);
-    int right = count(grid,a,b+1,m,n,dp);
-    
-    return dp[a][b]= down+right;
-  }*/
+    int solve(int[][]grid , int i , int j , int m ,int n , int[][]dp)
+    {
+        if(i>m || j >n || grid[i][j]==1)return 0;
+        if(i==m&&j==n)return 1;
+        if(dp[i][j]!=-1)return dp[i][j];
+        
+        int down = solve(grid,i+1,j,m,n,dp);
+        int right = solve(grid,i,j+1,m,n,dp);
+        return dp[i][j]=down+right;
+    }
 }
