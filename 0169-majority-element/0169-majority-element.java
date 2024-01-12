352b70aeff40a8709a81b8e8c4390483ef7e131a
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2024-01-12 13:20:10 +0530

    Time: 16 ms (16.12%), Space: 49.1 MB (42.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,45 +1,23 @@
 class Solution {
     public int majorityElement(int[] nums) {
-      
-      // HashMap solution 
-      
-   /*   TreeMap<Integer,Integer> map = new TreeMap<>();
-      int ans=-1;
-      for(int i=0 ; i<nums.length ; i++)
-      {
-        if(map.containsKey(nums[i]))
+        
+        // frequency count using hashmap 
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n=nums.length;
+        int ele = 0 ;
+        for(int i=0 ; i<n ; i++)
         {
-          
-          map.put(nums[i] , map.get(nums[i])+1);
+          map.put(nums[i], map.getOrDefault(nums[i],0)+1 );  
+            if(map.get(nums[i])>n/2)
+            {
+                ele = nums[i];
+                break;
+            }
         }
-    
-        else {  map.put(nums[i],0);
-             }
-        if(map.get(nums[i])>nums.length/2)
-        {
-          ans = nums[i];
-          break;
-        }
-      }
-
-      return ans; */
-      
-      // Moore's Voting algo 
-      
-      int count =1 ;
-      int ele = nums[0];
-      int n = nums.length;
-      
-      for(int i=1 ; i<n ; i++)
-      {
-        if(count==0) ele=nums[i];
         
-        if(ele == nums[i]) count++;
-        else count --;
+        return ele;
         
-        
-      }
-      return ele;
-      
+       
     }
 }
