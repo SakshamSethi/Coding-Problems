29cf9cb3c710fe9faa3001f0600c1f8207333347
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2024-01-12 20:15:05 +0530

    Time: 1074 ms (15.42%), Space: 67.2 MB (12.02%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,71 +1,29 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-     
-    // Wihtout using extra space :
-      /*if(nums.length==0)return 0;
-      
-      Arrays.sort(nums);
-      
-      int longest =  1;
-      int count= 0;
-      int lastSmaller = Integer.MIN_VALUE ;
-      
-      for(int ele : nums)
-      {
-        if(ele-1 == lastSmaller)
+        
+        HashSet<Integer> hs = new HashSet();
+        
+        for(int ele : nums) hs.add(ele);
+        
+        
+        int longest = 0 ;
+        
+        for(int ele : nums)
         {
-          count++;
-          lastSmaller = ele ;
+            
+            if(!hs.contains(ele-1))
+            {
+                int currentNum = ele ;
+                int currCount = 1;
+                while(hs.contains(currentNum+1))
+                {
+                    currentNum ++;
+                    currCount++;
+                }
+                longest = Math.max(longest , currCount);
+            }
+            
         }
-        
-        else if(ele == lastSmaller) continue ;
-        
-        else 
-        {
-          count = 1 ;
-          lastSmaller = ele ;
-        }
-        
-        longest = Math.max(longest , count );
-        
-      }
-      
-      return longest; */
-      
-      
-      
-      // using extra space for a set ;
-      
-      if(nums.length==0)
-        return 0;
-      
-      
-      
-      LinkedHashSet<Integer> set = new LinkedHashSet<>();
-      
-      for(int num : nums) set.add(num);
-      
-      int longest = 1;
-      
-      for(int num: set )
-      {
-        if(!set.contains(num-1))
-        {
-          int currNumber = num ;
-          int currCount = 1;
-          while(set.contains(currNumber+1))
-          {
-            currNumber++ ;
-            currCount++;
-          }
-        longest = Math.max(longest , currCount);   
-        }
-
-      }
-      
-      
-      return longest;
-      
-      
+        return longest;
     }
 }
