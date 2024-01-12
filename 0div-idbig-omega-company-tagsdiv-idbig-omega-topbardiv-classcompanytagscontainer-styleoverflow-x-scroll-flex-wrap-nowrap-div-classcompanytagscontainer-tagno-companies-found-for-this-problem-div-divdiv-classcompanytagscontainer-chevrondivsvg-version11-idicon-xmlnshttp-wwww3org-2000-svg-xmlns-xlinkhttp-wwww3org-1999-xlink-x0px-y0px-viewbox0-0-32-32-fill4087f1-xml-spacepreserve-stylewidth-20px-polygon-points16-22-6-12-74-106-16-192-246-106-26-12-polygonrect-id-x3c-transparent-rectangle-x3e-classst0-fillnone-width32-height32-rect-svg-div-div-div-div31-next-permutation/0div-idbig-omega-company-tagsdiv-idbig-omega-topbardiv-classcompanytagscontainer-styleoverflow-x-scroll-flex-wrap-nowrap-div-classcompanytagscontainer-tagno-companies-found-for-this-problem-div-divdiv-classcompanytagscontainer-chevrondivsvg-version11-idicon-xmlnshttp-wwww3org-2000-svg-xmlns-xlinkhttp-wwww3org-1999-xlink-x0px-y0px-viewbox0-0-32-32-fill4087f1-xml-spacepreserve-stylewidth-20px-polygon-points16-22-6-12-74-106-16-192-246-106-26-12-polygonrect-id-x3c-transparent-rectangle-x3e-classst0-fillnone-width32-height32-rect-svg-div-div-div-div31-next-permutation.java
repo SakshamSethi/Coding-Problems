89e77b1810c30b479caeb9dd6a339f1b01d277bd
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2024-01-12 19:40:54 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (19.57%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div31-next-permutation/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div31-next-permutation.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div31-next-permutation/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div31-next-permutation.java
@@ -1,8 +1,10 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         
-        int index = -1 ;
+        int index = -1;
         int n = nums.length;
+        // 1 : find the breaking point 
+        
         for(int i=n-2 ; i>=0 ; i--)
         {
             if(nums[i]<nums[i+1])
@@ -11,39 +13,42 @@ class Solution {
                 break;
             }
         }
-     
-        int min = Integer.MAX_VALUE;
-        int miniIdx = n+1;
-        if(index!=-1)
-        {
-             for(int i = n-1 ; i>=index ; i--)
-      {
-        if(nums[index]<nums[i])
-        {
-          int temp = nums[index];
-          nums[index] = nums[i];
-          nums[i]=temp;
-          break ;
-        }
-      }
-   
-            reverse(nums,index+1,n-1);
-        }
-        else
+        // check if -1 : if yes than this is the last permute so return its reverse as the first permute 
+        
+        if(index==-1)
         {
             reverse(nums,0,n-1);
+            return ;
         }
         
-    }
-    void reverse(int[]nums , int i , int j)
-    {
-        while(i<j)
+        // step 2 : Replace with immediate greater from back
+        
+        for(int i=n-1 ; i>=index ; i--)
         {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
+            if(nums[i]>nums[index])
+            {
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index]=temp;
+                break;
+            }
         }
+        
+        // step 3 : reverse the rest 
+        
+        reverse(nums,index+1 , n-1);
+        
+    }
+    
+    private void reverse(int[]nums , int i , int j)
+    {
+     while(i<j)
+     {
+         int temp = nums[i];
+         nums[i] = nums[j];
+         nums[j]=temp;
+         i++;
+         j--;
+     }
     }
 }
