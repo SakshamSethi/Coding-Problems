c0e2ebe2b517d3312997fb4277c04310bc6acef8
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2024-01-12 20:45:36 +0530

    Time: 1 ms (87.83%), Space: 45.2 MB (44.86%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div73-set-matrix-zeroes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div73-set-matrix-zeroes.java
@@ -0,0 +1,69 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+       
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        boolean row = false , col = false;
+        
+        for(int i=0 ; i<m ; i++)
+        {
+            if(matrix[i][0]==0)col=true;
+            
+        }
+        for(int i=0 ; i<n ; i++)
+        {
+            if(matrix[0][i]==0) row=true;
+        }
+        
+        
+        
+        for(int i=1 ; i<m ; i++)
+        {
+            for(int j=1 ; j<n ; j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        
+        
+        for(int i=1 ; i<n ; i++)
+        {
+            if(matrix[0][i]==0)
+            {
+                for(int j=0 ; j<m ; j++)
+                {
+                    matrix[j][i]=0;
+                }
+            }
+        }
+        
+        for(int i=1 ; i<m ; i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                for(int j=0 ; j<n ; j++)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+        if(row)
+        {
+            for(int i=0 ; i<n ; i++)
+                matrix[0][i]=0;
+        }
+        if(col)
+        {
+            for(int i=0 ; i<m ; i++)
+                matrix[i][0]=0;
+        }
+        
+        return;
+    }
+}
