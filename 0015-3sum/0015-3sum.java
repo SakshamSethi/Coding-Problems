5a26b082a9d289679b459436769bdf42b9af2561
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2024-01-13 19:28:52 +0530

    Time: 31 ms (69.83%), Space: 51.2 MB (59.38%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -36,7 +36,7 @@ class Solution {
        */
         
         
-        HashSet<Integer> temp ;
+        /*HashSet<Integer> temp ;
         HashSet<List<Integer>> hs = new HashSet();
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
@@ -67,6 +67,49 @@ class Solution {
         {
             ans.add(new ArrayList(list));
         }
-      return ans;  
+      return ans; */
+        
+        
+        // Optimal
+        
+        List<List<Integer>> ans = new ArrayList();
+        Arrays.sort(nums);
+        int n = nums.length;
+        
+        for(int i=0 ; i< n ; i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            
+            int j = i+1 ;
+            int k = n-1;
+            
+            while(j<k)
+            {
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum<0)
+                {
+                    j++;
+                }
+                else if(sum>0)
+                {
+                    k--;
+                }
+                if(sum==0)
+                {
+                    List<Integer>temp = new ArrayList();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    
+                    ans.add(new ArrayList(temp));
+                     j++;
+                     k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+                
+            }
+        }
+        return ans;
     }
 }
