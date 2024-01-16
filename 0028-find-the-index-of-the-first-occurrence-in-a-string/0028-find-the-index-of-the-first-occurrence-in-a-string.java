9a0a2a730706fda871ca8d0d33bf9a0d7e8f6f9f
SakshamSethi <118589149+SakshamSethi@users.noreply.github.com>
2024-01-16 11:53:03 +0530

    Time: 1 ms (38.00%), Space: 41.3 MB (32.01%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,25 +1,48 @@
 class Solution {
+    
+    private final int prime = 101 ;
+    
     public int strStr(String haystack, String needle) {
-    
-      return haystack.indexOf(needle);
-      
-     /* int n = haystack.length();
-      int m = needle.length();
-    
-       if(m>n)return -1; 
-      
-      for(int i=0 ; i<n-m+1;i++)
-      {
         
-        if(haystack.charAt(i)==needle.charAt(0))
+        return  haystack.length()>=needle.length()?search(haystack,needle):-1;
+    }
+    
+    private double calculateHash(String str)
+    {
+        double hash = 0 ;
+        for(int i=0 ; i<str.length();i++)
         {
-          if(haystack.substring(i,m+i).equals(needle))
-            return i;
+            hash+= str.charAt(i)*Math.pow(prime , i);
+        }
+        return hash;
+    }
+    private double updateHash(double prevHash , char prevChar ,char newChar , int patternLength)
+    {
+        double newHash = (prevHash-prevChar)/prime;
+        newHash += newChar*Math.pow(prime,patternLength-1);
+        return newHash;
+    }
+    
+    private int search(String text , String pattern)
+    {
+        int patternLen = pattern.length();
+        double patternHash = calculateHash(pattern);
+        double textHash = calculateHash(text.substring(0,patternLen));
+        
+        for(int i=0 ; i<=text.length()-patternLen;i++)
+        {
+            if(patternHash==textHash)
+            {
+                if(text.substring(i,i+patternLen).equals(pattern))
+                    return i;
+            }
+            if(i<text.length()-patternLen)
+            {
+                textHash = updateHash(textHash , text.charAt(i),text.charAt(i+patternLen),patternLen);
+            }
         }
         
-        
-      }
-      return -1 ;
-       */ 
+        return -1;
     }
+    
 }
